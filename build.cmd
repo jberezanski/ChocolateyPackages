4c7d5a47092f03a7240820785035527df0d01339
Jakub Bereżański <kuba@berezanscy.pl>
2014-02-12 01:18:22 +0100

    add rudimentary build script

---
+++ build.cmd
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+set ROOTDIR=%~dp0
+set OUTDIR=%ROOTDIR%\output
+if not exist "%OUTDIR%\." mkdir "%OUTDIR%"
+pushd "%OUTDIR%"
+for /f %%a in ('dir /s /b %ROOTDIR%\*.nuspec') do (
+	echo Packing %%~na
+	cpack %%a
+)
+popd
