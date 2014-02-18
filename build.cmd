ece2a376f213502170edfc6766d6cf5e826ebdad
Jakub Bereżański <kuba@berezanscy.pl>
2014-02-18 22:21:47 +0100

    enable building of selected package only

--- build.cmd
+++ build.cmd
@@ -1,11 +1,15 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 set ROOTDIR=%~dp0
 set OUTDIR=%ROOTDIR%\output
+set ONLYTHIS=%1
 if not exist "%OUTDIR%\." mkdir "%OUTDIR%"
 pushd "%OUTDIR%"
 for /f %%a in ('dir /s /b %ROOTDIR%\*.nuspec') do (
-	echo Packing %%~na
-	call cpack %%a
+	if "%1"=="" set ONLYTHIS=%%~na
+	if "!ONLYTHIS!"=="%%~na" (
+		echo Packing %%~na
+		call cpack %%a
+	)
 )
 popd
