3b9c47fe72178a56a2f8d80d43743052b23c54ff
Jakub Bereżański <kuba@berezanscy.pl>
2014-02-18 22:30:07 +0100

    javaruntime-preventasktoolbar: fix installation on 32-bit systems

--- javaruntime-preventasktoolbar/tools/chocolateyUninstall.ps1
+++ javaruntime-preventasktoolbar/tools/chocolateyUninstall.ps1
@@ -3,7 +3,7 @@ try
     $ErrorActionPreference = 'Stop'
     Set-StrictMode -Version 2
 
-    $is64 = Get-ProcessorBits -eq 64
+    $is64 = (Get-ProcessorBits) -eq 64
     if ($is64 -and ([IntPtr]::Size -eq 4)) {
         # no way to access 64-bit registry when running under WOW64 and PS 2.0 (.NET 2.0)
         throw "This package cannot be fully uninstalled using 32-bit PowerShell on a 64-bit system. Please uninstall this package using 64-bit PowerShell."
