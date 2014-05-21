054ce3633b1f4b3e14b88a25732ee960fc12c31b
Jakub Bereżański <kuba@berezanscy.pl>
2014-05-21 23:34:05 +0200

    fciv: extract to package directory, use builtin shim feature instead of custom path

--- fciv/tools/chocolateyInstall.ps1
+++ fciv/tools/chocolateyInstall.ps1
@@ -1,17 +1,36 @@
+# Download and extract fciv to package path. Chocolatey will automatically create a stub exe in a directory in PATH.
+Install-ChocolateyPackage 'fciv' 'exe' "/T:$($env:chocolateyPackageFolder) /Q" 'http://download.microsoft.com/download/c/f/4/cf454ae0-a4bb-4123-8333-a1b6737712f7/Windows-KB841290-x86-ENU.exe'
+
+# Cleanup after previous package version, which extracted to $Env:SystemDrive\tools\fciv
+# This should not influence the installation of this version, so catch and ignore all errors.
 try {
-  $binRoot = Join-Path $env:systemdrive 'tools'
-  $fcivPath = Join-Path $binRoot 'fciv'
+  if (Test-Path (Join-Path (Split-Path $env:chocolateyPackageFolder) 'fciv.1.0.0.0')) {
+    $fcivPath = Join-Path $Env:SystemDrive 'tools\fciv'
+    if (Test-Path $fcivPath) {
+      Write-Debug "Removing previous installation location $fcivPath"
+	  Remove-Item $fcivPath -Recurse -ErrorAction Continue
+    }
 
-  md -PATH $fcivPath
+    $userPath = [Environment]::GetEnvironmentVariable('PATH', 'User')
+    if ($userPath -ne $null -and $userPath -like "*$fcivPath*") {
+      Write-Debug "Removing previous installation location $fcivPath from the user PATH variable"
+      $newUserPathChunks = $userPath -split ';' | Where-Object { $_ -ne '' -and $_.TrimEnd('\') -ne $fcivPath }
+      $newUserPath = $newUserPathChunks -join ';'
+      if ($newUserPath -ne $userPath) {
+        [Environment]::SetEnvironmentVariable('PATH', $newUserPath, 'User')
+      }
+    }
 
-  # Download and extract fcivPath to installation path
-  Install-ChocolateyPackage 'fciv' 'exe' "/T:$($fcivPath) /Q" 'http://download.microsoft.com/download/c/f/4/cf454ae0-a4bb-4123-8333-a1b6737712f7/Windows-KB841290-x86-ENU.exe'
-
-  # Add fcivPath directory to PATH environment variable
-  Install-ChocolateyPath $fcivPath 'User'
-
-  Write-ChocolateySuccess 'fcivPath'
+    $currentPath = $Env:PATH
+    if ($currentPath -ne $null -and $currentPath -like "*$fcivPath*") {
+      Write-Debug "Removing previous installation location $fcivPath from the current Powershell PATH variable"
+      $newCurrentPathChunks = $currentPath -split ';' | Where-Object { $_ -ne '' -and $_.TrimEnd('\') -ne $fcivPath }
+      $newCurrentPath = $newCurrentPathChunks -join ';'
+      if ($newCurrentPath -ne $currentPath) {
+        $Env:PATH = $newCurrentPath
+      }
+    }
+  }
 } catch {
-  Write-ChocolateyFailure 'fcivPath' $($_.Exception.Message)
-  throw
+  Write-Warning "Failed to clean up after previous fciv package version: $($_.Exception.Message)"
 }
