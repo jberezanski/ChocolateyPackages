b31af729570a202654431b18a344ed7b46c381cd
Jakub Bereżański <kuba@berezanscy.pl>
2015-09-05 22:28:11 +0200

    add vs2012.5 and VS Update meta packages

---
+++ vs2012.5/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$arguments = @{
+    packageName = 'vs2012.5'
+    url = 'http://download.microsoft.com/download/1/7/A/17A8493D-BB25-4811-8242-CCCB74EF982E/VS2012.5.exe'
+    checksumType = 'sha1'
+    checksum = '577B762ED1FC3A7046840D6588E0CC4462ABBC0C'
+    installerType = 'exe'
+    silentArgs = "/Passive /NoRestart /Log ""${Env:TEMP}\VS2012.5.log"""
+    validExitCodes = @(0, 3010)
+}
+Install-ChocolateyPackage @arguments
