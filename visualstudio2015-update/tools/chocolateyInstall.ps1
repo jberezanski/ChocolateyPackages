eb1a763fce110c133a937e6d9cff31514aea5cd8
Jakub Bereżański <kuba@berezanscy.pl>
2015-12-06 23:02:21 +0100

    update VS 2015 Update 1 to RTM

--- visualstudio2015-update/tools/chocolateyInstall.ps1
+++ visualstudio2015-update/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$arguments = @{
-    packageName = 'VS2015.1 CTP'
-    url = 'https://download.microsoft.com/download/F/8/7/F87B8816-F2B5-45F0-A7D2-F0374B7AD2A9/VS2015.1%20CTP.exe'
+    packageName = 'VS2015.1'
+    url = 'https://download.microsoft.com/download/4/C/1/4C113E0D-8590-47AB-B7B8-E41E0AD7936D/VS2015.1.exe'
     checksumType = 'sha1'
-    checksum = 'D08A54ADD8128915361B9EEDDCA4DE952BA94977'
+    checksum = '4E1541FF941E837928CF5A6E1592E0E9AE5CC203'
     installerType = 'exe'
-    silentArgs = "/Passive /NoRestart /Log ""${Env:TEMP}\VS2015.1 CTP.log"""
+    silentArgs = "/Passive /NoRestart /Log ""${Env:TEMP}\VS2015.1.log"""
     validExitCodes = @(0, 3010)
 }
 Install-ChocolateyPackage @arguments
