f8be02d1aa5fd8925bd011aeb1677140ef025e62
Jakub Bereżański <kuba@berezanscy.pl>
2016-02-23 22:51:19 +0100

    visualstudio2015-update: update to Update 2 CTP

--- visualstudio2015-update/tools/chocolateyInstall.ps1
+++ visualstudio2015-update/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$arguments = @{
     packageName = 'visualstudio2015-update'
-    url = 'https://download.microsoft.com/download/4/C/1/4C113E0D-8590-47AB-B7B8-E41E0AD7936D/VS2015.1.exe'
+    url = 'https://download.microsoft.com/download/d/e/d/ded1709a-cda8-491d-ace6-0ef20c07502c/vs2015.2%20ctp.exe'
     checksumType = 'sha1'
-    checksum = '4E1541FF941E837928CF5A6E1592E0E9AE5CC203'
+    checksum = 'C4A9C7E16E78C3E063AE011DED3CB23FE6FBF733'
     installerType = 'exe'
     silentArgs = "/Passive /NoRestart /Log ""${Env:TEMP}\{$packageName}.log"""
     validExitCodes = @(0, 3010)
