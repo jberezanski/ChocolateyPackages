8f40f5b0fda465d77c284a21b85bcd852e0fa8d5
Jakub Bereżański <kuba@berezanscy.pl>
2016-02-23 22:50:49 +0100

    visualstudio2015-update: fix log file name and package name in diagnostic messages

--- visualstudio2015-update/tools/chocolateyInstall.ps1
+++ visualstudio2015-update/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$arguments = @{
-    packageName = 'VS2015.1'
+    packageName = 'visualstudio2015-update'
     url = 'https://download.microsoft.com/download/4/C/1/4C113E0D-8590-47AB-B7B8-E41E0AD7936D/VS2015.1.exe'
     checksumType = 'sha1'
     checksum = '4E1541FF941E837928CF5A6E1592E0E9AE5CC203'
     installerType = 'exe'
-    silentArgs = "/Passive /NoRestart /Log ""${Env:TEMP}\VS2015.1.log"""
+    silentArgs = "/Passive /NoRestart /Log ""${Env:TEMP}\{$packageName}.log"""
     validExitCodes = @(0, 3010)
 }
 Install-ChocolateyPackage @arguments
