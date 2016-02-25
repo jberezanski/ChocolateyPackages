85dd96134d20b1d2b2386c495a63d017f35ddd06
Jakub Bereżański <kuba@berezanscy.pl>
2016-02-25 11:51:45 +0100

    visualstudio2015-update: fix log file name (again), add CTP 2 version number

--- visualstudio2015-update/tools/chocolateyInstall.ps1
+++ visualstudio2015-update/tools/chocolateyInstall.ps1
@@ -1,10 +1,11 @@
-﻿$arguments = @{
+﻿$packageVersion = '2.0.0.0-ctp'
+$arguments = @{
     packageName = 'visualstudio2015-update'
     url = 'https://download.microsoft.com/download/d/e/d/ded1709a-cda8-491d-ace6-0ef20c07502c/vs2015.2%20ctp.exe'
     checksumType = 'sha1'
     checksum = 'C4A9C7E16E78C3E063AE011DED3CB23FE6FBF733'
     installerType = 'exe'
-    silentArgs = "/Passive /NoRestart /Log ""${Env:TEMP}\{$packageName}.log"""
+    silentArgs = "/Passive /NoRestart /Log ""${Env:TEMP}\${packageName}-${packageVersion}.log"""
     validExitCodes = @(0, 3010)
 }
 Install-ChocolateyPackage @arguments
