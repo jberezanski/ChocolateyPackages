f420e1399bc6e019e3c84676afa035eecd28973a
Jakub Bereżański <kuba@berezanscy.pl>
2016-04-01 18:41:18 +0200

    update VS 2015 Update 2 to RTM

--- visualstudio2015-update/tools/chocolateyInstall.ps1
+++ visualstudio2015-update/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-﻿$packageVersion = '2.0.0.0-rc'
+﻿$packageVersion = '2.0.0.0'
 $arguments = @{
     packageName = 'visualstudio2015-update'
-    url = 'http://download.microsoft.com/download/b/f/3/bf3d7cc0-4c16-4fcf-8533-15241da1de95/vs2015.2%20rc.exe'
+    url = 'https://download.microsoft.com/download/c/8/6/c868d99e-f6cb-4b6f-955e-4571614e6fdb/vs2015.2.exe'
     checksumType = 'sha1'
-    checksum = '475EA6234BD4E7292874FE0AE4A08FBD9BE10D67'
+    checksum = '82F2841B9206F5CD99590537A023943DD7C8ABCE'
     installerType = 'exe'
     silentArgs = "/Passive /NoRestart /Log ""${Env:TEMP}\${packageName}-${packageVersion}.log"""
     validExitCodes = @(0, 3010)
