29b6365a435b3c4566b0cf2a5a360585a593cd16
Jakub Bereżański <j.berezanski@medicalgorithmics.com>
2016-04-25 14:19:54 +0200

    vs15: implement uninstallation

--- visualstudio15enterprise/tools/ChocolateyUninstall.ps1
+++ visualstudio15enterprise/tools/ChocolateyUninstall.ps1
@@ -1,7 +1,6 @@
 ﻿Import-Module -Name (Join-Path -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition) -ChildPath 'VSServicing.psm1')
 
-throw 'TODO: update ApplicationName'
 Uninstall-VS `
     -PackageName 'visualstudio15enterprise' `
-    -ApplicationName 'Microsoft Visual Studio Enterprise "15" TODO' `
+    -ApplicationName 'Microsoft Visual Studio Enterprise 15 Preview' `
     -UninstallerName 'vs_enterprise.exe'
