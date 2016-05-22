0dfff972169db95e79912742463be16537be9483
Jakub Bereżański <kuba@berezanscy.pl>
2016-05-22 23:25:36 +0200

    vs15: detect reboot requirement also on older chocos
    This should work even on PowerShell-based Chocolatey (0.9.8.x).

--- visualstudio15enterprise/tools/VSServicing.psm1
+++ visualstudio15enterprise/tools/VSServicing.psm1
@@ -139,6 +139,149 @@ function Generate-InstallArgumentsString($parameters, $adminFile)
     return $s
 }
 
+if (-not (Test-Path -Path Function:\Set-PowerShellExitCode))
+{
+    # based on Set-PowerShellExitCode (07277ac), included here unchanged to add exit code support to old Chocolatey
+    function Set-PowerShellExitCode {
+        param (
+            [int]$exitCode
+        )
+
+        $host.SetShouldExit($exitCode); 
+        $env:ChocolateyExitCode = $exitCode;
+    }
+}
+
+# based on Start-ChocolateyProcessAsAdmin (8734611), included here renamed but otherwise unchanged to add exit code support to old Chocolatey
+function Start-VSChocolateyProcessAsAdmin {
+param(
+  [string] $statements,
+  [string] $exeToRun = 'powershell',
+  [switch] $minimized,
+  [switch] $noSleep,
+  $validExitCodes = @(0)
+)
+  Write-Debug "Running 'Start-VSChocolateyProcessAsAdmin' with exeToRun:`'$exeToRun`', statements: `'$statements`' ";
+
+  $wrappedStatements = $statements
+  if ($wrappedStatements -eq $null) { $wrappedStatements = ''}
+
+  if ($exeToRun -eq 'powershell') {
+    $exeToRun = "$($env:SystemRoot)\System32\WindowsPowerShell\v1.0\powershell.exe"
+    $importChocolateyHelpers = ""
+    Get-ChildItem "$helpersPath" -Filter *.psm1 | ForEach-Object { $importChocolateyHelpers = "& import-module -name  `'$($_.FullName)`';$importChocolateyHelpers" };
+    $block = @"
+      `$noSleep = `$$noSleep
+      $importChocolateyHelpers 
+      try{
+        `$progressPreference="SilentlyContinue"
+        $statements 
+        if(!`$noSleep){start-sleep 6}
+      }
+      catch{
+        if(!`$noSleep){start-sleep 8}
+        throw
+      }
+"@
+    $encoded = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($block))
+    $wrappedStatements = "-NoProfile -ExecutionPolicy bypass -EncodedCommand $encoded"
+    $dbgMessage = @"
+Elevating Permissions and running powershell block:
+$block 
+This may take a while, depending on the statements.
+"@
+  }
+  else 
+  {
+    $dbgMessage = @"
+Elevating Permissions and running [`"$exeToRun`" $wrappedStatements]. This may take a while, depending on the statements.
+"@
+  }
+  
+  Write-Debug $dbgMessage
+
+  $exeIsTextFile = [System.IO.Path]::GetFullPath($exeToRun) + ".istext"
+  if (([System.IO.File]::Exists($exeIsTextFile))) {
+    Set-PowerShellExitCode 4
+    throw "The file was a text file but is attempting to be run as an executable - '$exeToRun'"
+  }
+  
+  if ($exeToRun -eq 'msiexec' -or $exeToRun -eq 'msiexec.exe') {
+    $exeToRun = "$($env:SystemRoot)\System32\msiexec.exe"
+  }
+
+  if (!([System.IO.File]::Exists($exeToRun)) -and $exeToRun -notmatch 'msiexec') {
+    Write-Warning "May not be able to find '$exeToRun'. Please use full path for executables."
+    # until we have search paths enabled, let's just pass a warning
+    #Set-PowerShellExitCode 2
+    #throw "Could not find '$exeToRun'"
+  }
+
+  # Redirecting output slows things down a bit.
+  $writeOutput = {
+    if ($EventArgs.Data -ne $null) {
+      Write-Host "$($EventArgs.Data)"
+    }
+  }
+
+  $writeError = {
+    if ($EventArgs.Data -ne $null) {
+      Write-Error "$($EventArgs.Data)"
+    }
+  }
+
+  $process = New-Object System.Diagnostics.Process
+  $process.EnableRaisingEvents = $true
+  Register-ObjectEvent  -InputObject $process -SourceIdentifier "LogOutput_ChocolateyProc" -EventName OutputDataReceived -Action $writeOutput | Out-Null
+  Register-ObjectEvent -InputObject $process -SourceIdentifier "LogErrors_ChocolateyProc" -EventName ErrorDataReceived -Action  $writeError | Out-Null
+
+  #$process.StartInfo = New-Object System.Diagnostics.ProcessStartInfo($exeToRun, $wrappedStatements)
+  # in case empty args makes a difference, try to be compatible with the older
+  # version
+  $psi = New-Object System.Diagnostics.ProcessStartInfo
+  $psi.FileName = $exeToRun
+  if ($wrappedStatements -ne '') {
+    $psi.Arguments = "$wrappedStatements"
+  }
+  $process.StartInfo =  $psi
+
+  # process start info
+  $process.StartInfo.RedirectStandardOutput = $true
+  $process.StartInfo.RedirectStandardError = $true
+  $process.StartInfo.UseShellExecute = $false
+  $process.StartInfo.WorkingDirectory = Get-Location
+  if ([Environment]::OSVersion.Version -ge (New-Object 'Version' 6,0)){
+    Write-Debug "Setting RunAs for elevation"
+    $process.StartInfo.Verb = "RunAs"
+  }
+  if ($minimized) {
+    $process.StartInfo.WindowStyle = [System.Diagnostics.ProcessWindowStyle]::Minimized
+  }
+
+  $process.Start() | Out-Null
+  if ($process.StartInfo.RedirectStandardOutput) { $process.BeginOutputReadLine() }
+  if ($process.StartInfo.RedirectStandardError) { $process.BeginErrorReadLine() }
+  $process.WaitForExit()  
+
+  # For some reason this forces the jobs to finish and waits for
+  # them to do so. Without this it never finishes.
+  Unregister-Event -SourceIdentifier "LogOutput_ChocolateyProc"
+  Unregister-Event -SourceIdentifier "LogErrors_ChocolateyProc"
+
+  $exitCode = $process.ExitCode
+  $process.Dispose()
+
+  Write-Debug "Command [`"$exeToRun`" $wrappedStatements] exited with `'$exitCode`'."
+  if ($validExitCodes -notcontains $exitCode) {
+    Set-PowerShellExitCode $exitCode
+    throw "Running [`"$exeToRun`" $statements] was not successful. Exit code was '$exitCode'. See log for possible error messages."
+  }
+ 
+  Write-Debug "Finishing 'Start-VSChocolateyProcessAsAdmin'"
+
+  return $exitCode
+}
+
 # based on Install-ChocolateyPackage (a9519b5), with changes:
 # - added recognition of exit codes signifying reboot requirement
 # - VS installers are exe
@@ -219,7 +362,7 @@ function Install-VSChocolateyInstallPackage {
     if (($successExitCodes | Measure-Object).Count -gt 0) { $validExitCodes += $successExitCodes }
     if (($rebootExitCodes | Measure-Object).Count -gt 0) { $validExitCodes += $rebootExitCodes }
 
-    $exitCode = Start-ChocolateyProcessAsAdmin -statements $silentArgs -exeToRun $filePath -validExitCodes $validExitCodes
+    $exitCode = Start-VSChocolateyProcessAsAdmin -statements $silentArgs -exeToRun $filePath -validExitCodes $validExitCodes
     $needsReboot = $false
     if ($exitCode -ne $null)
     {
@@ -263,7 +406,7 @@ function Uninstall-VSChocolateyPackage
     if (($successExitCodes | Measure-Object).Count -gt 0) { $validExitCodes += $successExitCodes }
     if (($rebootExitCodes | Measure-Object).Count -gt 0) { $validExitCodes += $rebootExitCodes }
 
-    $exitCode = Start-ChocolateyProcessAsAdmin -statements $silentArgs -exeToRun $file -validExitCodes $validExitCodes
+    $exitCode = Start-VSChocolateyProcessAsAdmin -statements $silentArgs -exeToRun $file -validExitCodes $validExitCodes
     $needsReboot = $false
     if ($exitCode -ne $null)
     {
