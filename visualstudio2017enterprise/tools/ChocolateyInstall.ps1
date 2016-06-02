5753bcef4fb8675c12afd0c3627956ba87766bd6
Jakub Bereżański <kuba@berezanscy.pl>
2016-06-02 23:20:51 +0200

    vs15: rename to visualstudio2017enterprise
    The package id is just an educated guess based on the naming of the Web
    Platform Installer product VWDOrVs2017AzurePack - Microsoft Azure SDK for
    .NET (VS "15" Preview). There is no official confirmation that Visual
    Studio "15" will be released in 2017 or that it will be named Visual
    Studio 2017. However, it seems quite likely.

--- visualstudio2017enterprise/tools/ChocolateyInstall.ps1
+++ visualstudio2017enterprise/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿Import-Module -Name (Join-Path -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition) -ChildPath 'VSServicing.psm1')
 
 Install-VS `
-    -PackageName 'visualstudio15enterprise' `
+    -PackageName 'visualstudio2017enterprise' `
     -ApplicationName 'Microsoft Visual Studio Enterprise 15 Preview' `
     -Url 'https://download.microsoft.com/download/5/4/2/5421527f-7664-4d2c-aacb-d3b3f25d7143/vs_enterprise.exe' `
     -ChecksumSha1 '769DCA8568D9CF70BBD2DBBE95AF1FDB473F5E12'
--- visualstudio2017enterprise/tools/ChocolateyUninstall.ps1
+++ visualstudio2017enterprise/tools/ChocolateyUninstall.ps1
@@ -1,6 +1,6 @@
 ﻿Import-Module -Name (Join-Path -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition) -ChildPath 'VSServicing.psm1')
 
 Uninstall-VS `
-    -PackageName 'visualstudio15enterprise' `
+    -PackageName 'visualstudio2017enterprise' `
     -ApplicationName 'Microsoft Visual Studio Enterprise 15 Preview' `
     -UninstallerName 'vs_enterprise.exe'
