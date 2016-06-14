9d0e2866397d6ecbddd0c9c1354825afe67d99c6
Jakub Bereżański <kuba@berezanscy.pl>
2016-06-14 20:27:44 +0200

    add VS 2015 Update 3 RC

--- visualstudio2015-update/tools/chocolateyInstall.ps1
+++ visualstudio2015-update/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-﻿$packageVersion = '2.0.0.0'
+﻿$packageVersion = '3.0.0.0-rc'
 $arguments = @{
     packageName = 'visualstudio2015-update'
-    url = 'https://download.microsoft.com/download/c/8/6/c868d99e-f6cb-4b6f-955e-4571614e6fdb/vs2015.2.exe'
+    url = 'https://download.microsoft.com/download/9/4/f/94f9f500-fe47-4a4e-be00-85350e2cf87a/vs2015.3%20rc.exe'
     checksumType = 'sha1'
-    checksum = '82F2841B9206F5CD99590537A023943DD7C8ABCE'
+    checksum = 'EB3893A68C77F80806F3140BAEA9FEEB4093CC9F'
     installerType = 'exe'
     silentArgs = "/Passive /NoRestart /Log ""${Env:TEMP}\${packageName}-${packageVersion}.log"""
     validExitCodes = @(0, 3010)
