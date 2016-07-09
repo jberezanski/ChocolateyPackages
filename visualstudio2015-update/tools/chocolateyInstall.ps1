52afd84a4e88100a98f9fb25468ca521e9ead148
Jakub Bereżański <kuba@berezanscy.pl>
2016-07-09 23:02:26 +0200

    update visualstudio2015-update to Update 3 RTM

--- visualstudio2015-update/tools/chocolateyInstall.ps1
+++ visualstudio2015-update/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-﻿$packageVersion = '3.0.0.0-rc'
+﻿$packageVersion = '3.0.0.0'
 $arguments = @{
     packageName = 'visualstudio2015-update'
-    url = 'https://download.microsoft.com/download/9/4/f/94f9f500-fe47-4a4e-be00-85350e2cf87a/vs2015.3%20rc.exe'
+    url = 'https://download.microsoft.com/download/4/8/f/48f0645f-51b6-4733-b808-63e640cddaec/vs2015.3.exe'
     checksumType = 'sha1'
-    checksum = 'EB3893A68C77F80806F3140BAEA9FEEB4093CC9F'
+    checksum = '68ABF90424AFF604A04D6C61FB52ADCD2CAB2266'
     installerType = 'exe'
     silentArgs = "/Passive /NoRestart /Log ""${Env:TEMP}\${packageName}-${packageVersion}.log"""
     validExitCodes = @(0, 3010)
