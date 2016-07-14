0dd6210067c745fed1967d9f3459028f2114a48d
Jakub Bereżański <kuba@berezanscy.pl>
2016-07-14 23:01:24 +0200

    copytovm: fix default parameter value, copy all files

--- output/copytovm.ps1
+++ output/copytovm.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding()]
 Param (
-  [string[]] $VMName = (Get-VM | ? State -eq Running)
+  [string[]] $VMName = (Get-VM | ? State -eq Running | select -ExpandProperty Name)
 )
 
-gci *.nupkg | % { copy-vmfile $VMName $_.FullName ('C:\ChocoSource\' + $_.Name) -fi host -fo }
+gci *.* | % { copy-vmfile $VMName $_.FullName ('C:\ChocoSource\' + $_.Name) -fi host -fo }
