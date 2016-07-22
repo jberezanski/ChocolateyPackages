1797e53500babd3190a263084a45dd04bfd4e506
Jakub Bereżański <kuba@berezanscy.pl>
2016-07-23 00:02:21 +0200

    dotnetcore-windowshosting: pass explicit 64-bit urls so that Chocolatey reports correct bitness being installed

--- dotnetcore-windowshosting/tools/data.ps1
+++ dotnetcore-windowshosting/tools/data.ps1
@@ -4,9 +4,9 @@
     Url = 'https://download.microsoft.com/download/A/3/8/A38489F3-9777-41DD-83F8-2CBDFAB2520C/DotNetCore.1.0.0-WindowsHosting.exe'
     Checksum = 'A9291EFEF99D1246CEA8A80DE776DBCEB40EE3CC'
     ChecksumType = 'sha1'
-    Url64 = $null
-    Checksum64 = $null
-    ChecksumType64 = $null
+    Url64 = 'https://download.microsoft.com/download/A/3/8/A38489F3-9777-41DD-83F8-2CBDFAB2520C/DotNetCore.1.0.0-WindowsHosting.exe'
+    Checksum64 = 'A9291EFEF99D1246CEA8A80DE776DBCEB40EE3CC'
+    ChecksumType64 = 'sha1'
     ApplicationName = 'Microsoft .NET Core 1.0.0 - Windows Server Hosting'
     UninstallerName = 'DotNetCore.1.0.0-WindowsHosting.exe'
     AdditionalInstallerArguments = 'OPT_INSTALL_REDIST=0'
