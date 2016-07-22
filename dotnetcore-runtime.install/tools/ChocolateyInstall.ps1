31c78716e4b08f2fca8e7283d410e7b56be4baab
Jakub Bereżański <kuba@berezanscy.pl>
2016-07-22 22:38:01 +0200

    dotnetcore-runtime.install: sync install logic with dotnetcore-windowshosting

--- dotnetcore-runtime.install/tools/ChocolateyInstall.ps1
+++ dotnetcore-runtime.install/tools/ChocolateyInstall.ps1
@@ -1,33 +1,43 @@
 ﻿$ErrorActionPreference = 'Stop'
 Set-StrictMode -Version 2
 
+$data = & (Join-Path -Path (Split-Path -Path $MyInvocation.MyCommand.Path) -ChildPath data.ps1)
+
 function Test-QuietRequested
 {
     return $Env:chocolateyPackageParameters -like '*Quiet*'
 }
 
-if (Test-QuietRequested) {
-    Write-Verbose 'Performing a quiet installation, as requested.'
-    $passiveOrQuiet = 'quiet'
-} else {
-    Write-Verbose 'Performing an installation with visible progress window (default).'
-    $passiveOrQuiet = 'passive'
+function Get-PassiveOrQuietArgument
+{
+    [CmdletBinding()]
+    Param (
+        [string] $Scenario = 'installation'
+    )
+    if (Test-QuietRequested) {
+        Write-Verbose "Performing a quiet $Scenario, as requested."
+        $passiveOrQuiet = 'quiet'
+    } else {
+        Write-Verbose "Performing an $Scenario with visible progress window (default)."
+        $passiveOrQuiet = 'passive'
+    }
+    return $passiveOrQuiet
 }
 
-$packageName = 'dotnetcore-runtime.install'
+$passiveOrQuiet = Get-PassiveOrQuietArgument -Scenario 'installation'
 $arguments = @{
-    packageName = $packageName
-    silentArgs = "/$passiveOrQuiet /norestart /log ""${Env:TEMP}\${packageName}.log"""
+    packageName = $data.PackageName
+    silentArgs = "$($data.AdditionalInstallerArguments) /install /$passiveOrQuiet /norestart /log ""${Env:TEMP}\$($data.PackageName).log"""
     validExitCodes = @(
         0, # success
         3010 # success, restart required
     )
-    url = 'https://download.microsoft.com/download/A/3/8/A38489F3-9777-41DD-83F8-2CBDFAB2520C/packages/DotNetCore.1.0.0-Runtime-x86.exe'
-    checksum = '88581A972CBBA8CD0954987FDDCC274212EB315D'
-    checksumType = 'sha1'
-    url64 = 'https://download.microsoft.com/download/A/3/8/A38489F3-9777-41DD-83F8-2CBDFAB2520C/packages/DotNetCore.1.0.0-Runtime-x64.exe'
-    checksum64 = '0953E863F28809901663BD86B0150EAE977A31F4'
-    checksumType64 = 'sha1'
+    url = $data.Url
+    checksum = $data.Checksum
+    checksumType = $data.ChecksumType
+    url64 = $data.Url64
+    checksum64 = $data.Checksum64
+    checksumType64 = $data.ChecksumType64
 }
 
 Install-ChocolateyPackage @arguments
---
+++ dotnetcore-runtime.install/tools/data.ps1
@@ -0,0 +1,11 @@
+﻿@{
+    PackageName = 'dotnetcore-runtime.install'
+    Version = '1.0.0'
+    Url = 'https://download.microsoft.com/download/A/3/8/A38489F3-9777-41DD-83F8-2CBDFAB2520C/packages/DotNetCore.1.0.0-Runtime-x86.exe'
+    Checksum = '88581A972CBBA8CD0954987FDDCC274212EB315D'
+    ChecksumType = 'sha1'
+    Url64 = 'https://download.microsoft.com/download/A/3/8/A38489F3-9777-41DD-83F8-2CBDFAB2520C/packages/DotNetCore.1.0.0-Runtime-x64.exe'
+    Checksum64 = '0953E863F28809901663BD86B0150EAE977A31F4'
+    ChecksumType64 = 'sha1'
+    AdditionalInstallerArguments = $null
+}
