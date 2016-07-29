cb046d6c591d6cb41d75c1d509d34483d7cde96d
Jakub Bereżański <kuba@berezanscy.pl>
2016-07-29 02:00:30 +0200

    implement dotnetcore-runtime.portable

---
+++ dotnetcore-runtime.portable/tools/ChocolateyInstall.ps1
@@ -0,0 +1,71 @@
+﻿$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version 2
+
+$data = & (Join-Path -Path (Split-Path -Path $MyInvocation.MyCommand.Path) -ChildPath data.ps1)
+
+function Test-ShouldShimDotnetExe
+{
+    return $Env:chocolateyPackageParameters -like '*ShimDotnetExe*'
+}
+
+function Test-SideBySide
+{
+    Param
+    (
+        [string] $packageDir,
+        [string] $packageName,
+        [string] $version
+    )
+
+    return $packageDir -like "*\$packageName.$version*"
+}
+
+$packageDir = Split-Path -Parent -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition)
+$sxs = Test-SideBySide -packageDir $packageDir -packageName $data.PackageName -version $data.Version
+if ($sxs)
+{
+    Write-Verbose 'Performing a side-by-side installation.'
+}
+else
+{
+    Write-Verbose 'Performing a non-side-by-side installation'
+}
+
+$shouldShimDotnetExe = Test-ShouldShimDotnetExe
+if ($shouldShimDotnetExe)
+{
+    Write-Output "A shim for dotnet.exe will be generated, as requested. The 'dotnet' command will be available from all command prompts."
+    if ($sxs)
+    {
+        Write-Warning 'In a side-by-side installation, only the last installed dotnet.exe will be accessible via the shim.'
+    }
+}
+else
+{
+    Write-Output "A shim for dotnet.exe will not be generated. The 'dotnet' command will need to be invoked using the full path ($packageDir\dotnet.exe)."
+}
+
+$arguments = @{
+    packageName = $data.PackageName
+    unzipLocation = $packageDir
+    url = $data.Url
+    checksum = $data.Checksum
+    checksumType = $data.ChecksumType
+    url64bit = $data.Url64
+    checksum64 = $data.Checksum64
+    checksumType64 = $data.ChecksumType64
+}
+Install-ChocolateyZipPackage @arguments | Out-Null # returns unzipLocation
+
+Write-Debug "Generating ShimGen .ignore files"
+$rootDotnetExePath = Join-Path -Path $packageDir -ChildPath 'dotnet.exe'
+Get-ChildItem -Path $packageDir -Include '*.exe' -Recurse | ForEach-Object -Process {
+    $filePath = $_.FullName
+    Write-Debug "Found exe file: $filePath"
+    if ($filePath -ne $rootDotnetExePath -or (-not $shouldShimDotnetExe))
+    {
+        $ignoreFilePath = "$filePath.ignore"
+        Write-Debug "Creating file $ignoreFilePath"
+        New-Item -Path $ignoreFilePath -ItemType File -Force | Out-Null
+    }
+}
---
+++ dotnetcore-runtime.portable/tools/data.ps1
@@ -0,0 +1,10 @@
+﻿@{
+    PackageName = 'dotnetcore-runtime.portable'
+    Version = '1.0.0'
+    Url = 'https://download.microsoft.com/download/8/4/E/84EA9F4F-0E3A-4B41-A18A-36D51B06CBED/dotnet-win-x86.1.0.0.zip'
+    Checksum = '4F5A4CEF9BCEB2D66613F3C4163B69C0111ED163'
+    ChecksumType = 'sha1'
+    Url64 = 'https://download.microsoft.com/download/8/4/E/84EA9F4F-0E3A-4B41-A18A-36D51B06CBED/dotnet-win-x64.1.0.0.zip'
+    Checksum64 = 'D96C58EED3C6E40E066E38C5ABF6DEC71F85CCDF'
+    ChecksumType64 = 'sha1'
+}
