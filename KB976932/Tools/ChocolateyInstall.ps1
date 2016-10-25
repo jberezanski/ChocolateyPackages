3a4117afb296489241ffacd2441a11c8ee3bcac6
Jakub Bereżański <kuba@berezanscy.pl>
2016-10-26 01:07:51 +0200

    KB976932: recognize 'already installed' exit code; improve messages and description

--- KB976932/Tools/ChocolateyInstall.ps1
+++ KB976932/Tools/ChocolateyInstall.ps1
@@ -15,11 +15,14 @@ if (Get-WmiObject -Class Win32_QuickFixEngineering -Filter ('HotFixID = "{0}"' -
     return
 }
 
-if ((Get-WmiObject -Class Win32_OperatingSystem).ServicePackMajorVersion -ge 1)
+$os = Get-WmiObject -Class Win32_OperatingSystem
+if ($os.ServicePackMajorVersion -ge 1)
 {
     Write-Host "Skipping installation because Service Pack 1 or later is already installed."
     return
 }
+
+Write-Host "Installing $($os.Caption) Service Pack 1 ($kb)"
 
 Write-Warning 'TODO: languages!'
 
@@ -27,8 +30,6 @@ $url = "https://download.microsoft.com/download/0/A/F/0AFB5316-3062-494A-AB78-7F
 $url64 = "https://download.microsoft.com/download/0/A/F/0AFB5316-3062-494A-AB78-7FB0D4461357/windows6.1-KB976932-X64.exe"
 $checksum = 'E5449839955A22FC4DD596291AFF1433B998F9797E1C784232226ABA1F8ABD97'
 $checksum64 = 'F4D1D418D91B1619688A482680EE032FFD2B65E420C6D2EAECF8AA3762AA64C8'
-
-Write-Warning 'TODO: check if exit code for "not needeed" is 0x80240017'
 
 Install-ChocolateyPackage `
     -PackageName $kb `
@@ -40,4 +41,4 @@ Install-ChocolateyPackage `
     -ChecksumType 'sha256' `
     -Checksum64 $checksum64 `
     -ChecksumType64 'sha256' `
-    -ValidExitCodes @(0, 3010)
+    -ValidExitCodes @(0, 3010, 985603) # 0xf0a03 = 985603 = already installed
