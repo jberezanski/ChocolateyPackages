ff1cabec82b09b7af7883472ae6368349ca98fce
Jakub Bereżański <kuba@berezanscy.pl>
2017-01-26 21:37:08 +0100

    chocolatey-windowsupdate.extension: maintain full control over which functions are exported

--- chocolatey-windowsupdate.extension/extensions/chocolatey-windowsupdate.psm1
+++ chocolatey-windowsupdate.extension/extensions/chocolatey-windowsupdate.psm1
@@ -1,9 +1,9 @@
 $scriptRoot = Split-Path -Path $MyInvocation.MyCommand.Definition
 
-# Export functions that start with capital letter, others are private
-# Include file names that start with capital letters, ignore others
-$pre = Get-ChildItem -Path Function:\*
-Get-ChildItem -Path "$scriptRoot\*.ps1" | Where-Object { $_.Name -cmatch '^[A-Z]+' } | ForEach-Object { . $_ }
-$post = Get-ChildItem -Path Function:\*
-$funcs = Compare-Object -ReferenceObject $pre -DifferenceObject $post | Select-Object -ExpandProperty InputObject | Select-Object -ExpandProperty Name
-$funcs | Where-Object { $_ -cmatch '^[A-Z]+'} | ForEach-Object { Export-ModuleMember -Function $_ }
+$publicFunctions = @(
+    'Test-WindowsUpdate',
+    'Install-WindowsUpdate'
+)
+
+Get-ChildItem -Path "$scriptRoot\*.ps1" | ForEach-Object { . $_ }
+Export-ModuleMember -Function $publicFunctions
