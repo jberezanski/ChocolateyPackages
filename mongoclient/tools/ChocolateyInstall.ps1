4b80488ee97d6bc98755f0e4fadeb762b9c44d30
Jakub Bereżański <kuba@berezanscy.pl>
2017-02-07 12:15:31 +0100

    add mongoclient

---
+++ mongoclient/tools/ChocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version 2
+
+$url = 'https://github.com/rsercano/mongoclient/releases/download/1.5.0/windows-portable-x64.zip'
+$checksum = '9EBB529E24B1B5B73DC9C269EEC5A6A4BBC59EEAAAEB0DE2A4A30C44B21A240F'
+
+$packageArgs = @{
+    PackageName    = 'mongoclient'
+    UnzipLocation  = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+    Url64bit       = $url
+    Checksum64     = $checksum
+    ChecksumType64 = 'sha256'
+}
+
+Set-StrictMode -Off
+Install-ChocolateyZipPackage @packageArgs
