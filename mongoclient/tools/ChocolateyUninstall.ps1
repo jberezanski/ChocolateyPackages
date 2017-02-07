0ba56aaa19defc22bbe143f9c7a7114b168967b6
Jakub Bereżański <kuba@berezanscy.pl>
2017-02-07 20:23:22 +0100

    mongoclient: remove Start Menu items when uninstalling

---
+++ mongoclient/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version 2
+
+$specialFolders = @([System.Environment+SpecialFolder]::Programs)
+if ((Test-ProcessAdminRights) -and ([System.Environment+SpecialFolder] | Get-Member -Static -Name 'CommonPrograms') -ne $null)
+{
+    $specialFolders += [System.Environment+SpecialFolder]::CommonPrograms
+}
+
+foreach ($shortcutSpecialFolder in $specialFolders)
+{
+    $shortcutFilePath = Join-Path -Path ([Environment]::GetFolderPath($shortcutSpecialFolder)) -ChildPath 'Mongoclient.lnk'
+    if (Test-Path -Path $shortcutFilePath)
+    {
+        Write-Verbose "Removing shortcut file $shortcutFilePath"
+        Remove-Item -Path $shortcutFilePath -ErrorAction 'Continue'
+    }
+}
