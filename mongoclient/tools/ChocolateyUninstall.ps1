97b77de431d67b88b9325303f175f99ff78c6e07
Jakub Bereżański <kuba@berezanscy.pl>
2017-02-08 22:20:45 +0100

    mongoclient: do not leave 300+ MB of stuff after uninstall

--- mongoclient/tools/ChocolateyUninstall.ps1
+++ mongoclient/tools/ChocolateyUninstall.ps1
@@ -16,3 +16,14 @@ foreach ($shortcutSpecialFolder in $specialFolders)
         Remove-Item -Path $shortcutFilePath -ErrorAction 'Continue'
     }
 }
+
+$packageRootPath = Split-Path -Parent -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition)
+foreach ($dir in @('locales', 'resources'))
+{
+    $dirPath = Join-Path -Path $packageRootPath -ChildPath $dir
+    if (Test-Path -Path $dirPath)
+    {
+        Write-Verbose "Removing directory: $dirPath"
+        Remove-Item -Path $dirPath -Recurse -ErrorAction Continue
+    }
+}
