c25b7a51cc4c3905c9d10d982c79f06623ccab96
Jakub Bereżański <kuba@berezanscy.pl>
2017-02-10 22:45:48 +0100

    visualstudio2017enterprise: update to RC 6 (2017-02-07)

--- visualstudio2017enterprise/tools/ChocolateyInstall.ps1
+++ visualstudio2017enterprise/tools/ChocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 Install-VS `
     -PackageName 'visualstudio2017enterprise' `
-    -ApplicationName 'Microsoft Visual Studio Enterprise 15 Preview 5' `
-    -Url 'https://download.microsoft.com/download/D/1/5/D15E2DBA-ED51-487F-8CC0-4126D90043FB/vs_Enterprise.exe' `
-    -ChecksumSha1 'EF62172B0B8394BE9F1B52DB9986EF982209BBF1' `
+    -ApplicationName 'Microsoft Visual Studio Enterprise 2017 RC' `
+    -Url 'https://download.microsoft.com/download/4/2/9/429C6D6F-543E-4BB4-A2C7-4EFA7F8DE59D/vs_Enterprise.exe' `
+    -ChecksumSha1 '493364F350657B537077E72E7400DBF8875CD773' `
     -AssumeNewVS15Installer `
     -InstallerDisplayName 'Microsoft Visual Studio Installer'
--- visualstudio2017enterprise/tools/ChocolateyUninstall.ps1
+++ visualstudio2017enterprise/tools/ChocolateyUninstall.ps1
@@ -2,7 +2,7 @@
 
 Uninstall-VS `
     -PackageName 'visualstudio2017enterprise' `
-    -ApplicationName 'Microsoft Visual Studio Enterprise 15 Preview 5' `
+    -ApplicationName 'Microsoft Visual Studio Enterprise 2017 RC' `
     -UninstallerName 'vs_installer.exe' `
     -AssumeNewVS15Installer `
     -InstallerDisplayName 'Microsoft Visual Studio Installer'
