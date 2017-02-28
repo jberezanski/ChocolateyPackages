17d75d9a4a1e1c9be89d933e587c9c3277c414d9
Jakub Bereżański <kuba@berezanscy.pl>
2017-02-28 22:05:40 +0100

    visualstudio2017buildtools: uninstall only Build Tools, not all VS 2017 products

--- visualstudio2017buildtools/tools/ChocolateyUninstall.ps1
+++ visualstudio2017buildtools/tools/ChocolateyUninstall.ps1
@@ -1,6 +1,4 @@
-﻿Uninstall-VisualStudio `
+﻿Remove-VisualStudioProduct `
     -PackageName 'visualstudio2017buildtools' `
-    -ApplicationName 'Microsoft Visual Studio Build Tools 2017 RC' `
-    -UninstallerName 'vs_installer.exe' `
-    -InstallerTechnology 'WillowVS2017OrLater' `
-    -ProgramsAndFeaturesDisplayName 'Microsoft Visual Studio 2017'
+    -Product 'BuildTools' `
+    -VisualStudioYear '2017'
