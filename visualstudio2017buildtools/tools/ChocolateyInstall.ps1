7a32eebff247e8684e08644c267fe2cd50e77a2a
Jakub Bereżański <kuba@berezanscy.pl>
2017-02-28 07:05:11 +0100

    add visualstudio2017buildtools

---
+++ visualstudio2017buildtools/tools/ChocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿Install-VisualStudio `
+    -PackageName 'visualstudio2017buildtools' `
+    -ApplicationName 'Microsoft Visual Studio Build Tools 2017 RC' `
+    -Url 'https://download.microsoft.com/download/6/2/3/6238C02B-A5EA-46D8-8F31-3B8E0F54DD3B/vs_BuildTools.exe' `
+    -Checksum '426A59B0ACDB8CB553BF44182EFD273D0E1672D870A622FF581DFE7AA53C1231' `
+    -ChecksumType 'SHA256' `
+    -InstallerTechnology 'WillowVS2017OrLater' `
+    -ProgramsAndFeaturesDisplayName 'Microsoft Visual Studio 2017'
---
+++ visualstudio2017buildtools/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,6 @@
+﻿Uninstall-VisualStudio `
+    -PackageName 'visualstudio2017buildtools' `
+    -ApplicationName 'Microsoft Visual Studio Build Tools 2017 RC' `
+    -UninstallerName 'vs_installer.exe' `
+    -InstallerTechnology 'WillowVS2017OrLater' `
+    -ProgramsAndFeaturesDisplayName 'Microsoft Visual Studio 2017'
