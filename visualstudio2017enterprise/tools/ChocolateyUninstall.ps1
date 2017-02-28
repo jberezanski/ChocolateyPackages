b96a76e3dfa9e33764fedec25d4faaf5eab7afa5
Jakub Bereżański <kuba@berezanscy.pl>
2017-02-28 22:04:53 +0100

    visualstudio2017enterprise: uninstall only Enterprise, not all VS 2017 products

--- visualstudio2017enterprise/tools/ChocolateyUninstall.ps1
+++ visualstudio2017enterprise/tools/ChocolateyUninstall.ps1
@@ -1,6 +1,4 @@
-﻿Uninstall-VisualStudio `
+﻿Remove-VisualStudioProduct `
     -PackageName 'visualstudio2017enterprise' `
-    -ApplicationName 'Microsoft Visual Studio Enterprise 2017 RC' `
-    -UninstallerName 'vs_installer.exe' `
-    -InstallerTechnology 'WillowVS2017OrLater' `
-    -ProgramsAndFeaturesDisplayName 'Microsoft Visual Studio 2017'
+    -Product 'Enterprise' `
+    -VisualStudioYear '2017'
