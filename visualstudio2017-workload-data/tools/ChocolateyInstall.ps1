f02e2e55c5d1cf5df8c39cb01ac2ee7297c5d250
Jakub Bereżański <kuba@berezanscy.pl>
2017-03-03 23:48:00 +0100

    visualstudio2017-workload-data: install recommended components by default

--- visualstudio2017-workload-data/tools/ChocolateyInstall.ps1
+++ visualstudio2017-workload-data/tools/ChocolateyInstall.ps1
@@ -2,4 +2,5 @@
     -PackageName 'visualstudio2017-workload-data' `
     -Workload 'Data' `
     -VisualStudioYear '2017' `
-    -ApplicableProducts @('Community', 'Professional', 'Enterprise')
+    -ApplicableProducts @('Community', 'Professional', 'Enterprise') `
+    -IncludeRecommendedComponentsByDefault
