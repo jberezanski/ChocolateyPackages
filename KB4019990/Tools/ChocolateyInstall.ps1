0ee6fc781c56a5ef691c944f36628f290bc0d18a
Jakub Bereżański <kuba@berezanscy.pl>
2017-05-04 21:42:12 +0200

    add dotnet4.7, netfx-4.7-devpack and KB4019990

---
+++ KB4019990/Tools/ChocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$msuData = @{
+    '6.2-server' = @{
+        Url64 = 'https://download.microsoft.com/download/2/F/4/2F4F48F4-D980-43AA-906A-8FFF40BCB832/Windows8-RT-KB4019990-x64.msu'
+        Checksum64 = 'F50EFBD614094EBE84B0BCCB0F89903E5619E5A380755D0E8170E8E893AF7A9F'
+    }
+    '6.1' = @{
+        Url = 'https://download.microsoft.com/download/2/F/4/2F4F48F4-D980-43AA-906A-8FFF40BCB832/Windows6.1-KB4019990-x86.msu'
+        Checksum = '62101125E4619575A55A4FF63D049DEBD33E04B485B6616058862C525050E210'
+        Url64 = 'https://download.microsoft.com/download/2/F/4/2F4F48F4-D980-43AA-906A-8FFF40BCB832/Windows6.1-KB4019990-x64.msu'
+        Checksum64 = '4EE562192CF21716F3C38CAC3C2B17EF73B76708001D8A075D31DF0996F0C6B3'
+    }
+}
+
+$servicePackRequirements = @{
+    '6.1' = @{ ServicePackNumber = 1; ChocolateyPackage = 'KB976932' }
+}
+
+Install-WindowsUpdate -Id 'KB4019990' -MsuData $msuData -ChecksumType 'sha256' -ServicePackRequirements $servicePackRequirements
---
+++ netfx-4.7-devpack/tools/ChocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = 'netfx-4.7-devpack'
+$url = 'https://download.microsoft.com/download/A/1/D/A1D07600-6915-4CB8-A931-9A980EF47BB7/NDP47-DevPack-KB3186612-ENU.exe'
+$checksum = '16346BD9C464AE6439BD702702D5377BEB1B623683A4415DB5DBD3160318F625'
+$checksumType = 'sha256'
+$arguments = @{
+    packageName = $packageName
+    silentArgs = "/Quiet /NoRestart /Log ""${Env:TEMP}\${packageName}.log"""
+    validExitCodes = @(
+        0, # success
+        3010 # success, restart required
+    )
+    url = $url
+    checksum = $checksum
+    checksumType = $checksumType
+    url64 = $url
+    checksum64 = $checksum
+    checksumType64 = $checksumType
+}
+
+Install-ChocolateyPackage @arguments
