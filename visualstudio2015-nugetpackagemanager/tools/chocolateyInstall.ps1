6071899f349cefbabdc48341291826b362c4089e
Jakub Bereżański <kuba@berezanscy.pl>
2017-05-08 22:31:16 +0200

    fix arguments for vsix packages

--- visualstudio2015-nugetpackagemanager/tools/chocolateyInstall.ps1
+++ visualstudio2015-nugetpackagemanager/tools/chocolateyInstall.ps1
@@ -1,8 +1,9 @@
 ﻿$packageArgs = @{
-  packageName   = 'visualstudio2015-nugetpackagemanager'
-  url           = 'https://dist.nuget.org/visualstudio-2015-vsix/v3.5.0/NuGet.Tools.vsix'
-  checksum      = '785545171FBD226BEF88A1F4FFED39DFA6FE98AA1E254F1748F9C60DFA620D99'
-  checksumType  = 'sha256'
+  PackageName   = 'visualstudio2015-nugetpackagemanager'
+  VsVersion     = 14
+  VsixUrl       = 'https://dist.nuget.org/visualstudio-2015-vsix/v3.5.0/NuGet.Tools.vsix'
+  Checksum      = '785545171FBD226BEF88A1F4FFED39DFA6FE98AA1E254F1748F9C60DFA620D99'
+  ChecksumType  = 'sha256'
 }
 
 Install-ChocolateyVsixPackage @packageArgs
--- visualstudio2015-powershelltools/tools/chocolateyInstall.ps1
+++ visualstudio2015-powershelltools/tools/chocolateyInstall.ps1
@@ -1,8 +1,9 @@
 ﻿$packageArgs = @{
-  packageName   = 'visualstudio2015-powershelltools'
-  url           = 'https://visualstudiogallery.msdn.microsoft.com/c9eb3ba8-0c59-4944-9a62-6eee37294597/file/199313/5/PowerShellTools.14.0.vsix'
-  checksum      = 'D340BE9AC8C726C3050B631EFDB8830ABE60BD89D7652D5874E8E780407C04A1'
-  checksumType  = 'sha256'
+  PackageName   = 'visualstudio2015-powershelltools'
+  VsVersion     = 14
+  VsixUrl       = 'https://visualstudiogallery.msdn.microsoft.com/c9eb3ba8-0c59-4944-9a62-6eee37294597/file/199313/5/PowerShellTools.14.0.vsix'
+  Checksum      = 'D340BE9AC8C726C3050B631EFDB8830ABE60BD89D7652D5874E8E780407C04A1'
+  ChecksumType  = 'sha256'
 }
 
 Install-ChocolateyVsixPackage @packageArgs
