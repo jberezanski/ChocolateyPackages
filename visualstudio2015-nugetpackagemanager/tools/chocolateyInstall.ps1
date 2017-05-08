71efcebf526653a2f8678ff7dac2b2c6aac01c8a
Jakub Bereżański <kuba@berezanscy.pl>
2017-05-08 15:55:28 +0200

    add visualstudio2015-nugetpackagemanager

---
+++ visualstudio2015-nugetpackagemanager/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName   = 'visualstudio2015-nugetpackagemanager'
+  url           = 'https://dist.nuget.org/visualstudio-2015-vsix/v3.5.0/NuGet.Tools.vsix'
+  checksum      = '785545171FBD226BEF88A1F4FFED39DFA6FE98AA1E254F1748F9C60DFA620D99'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyVsixPackage @packageArgs
