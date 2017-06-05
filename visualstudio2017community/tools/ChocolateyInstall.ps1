567f7af69a7f1151bcf2ec71ae3d7ceae9d008fd
Jakub Bereżański <kuba@berezanscy.pl>
2017-06-05 21:54:46 +0200

    VS 2017 Community/Professional: fix weird spaces
    Apparently, those strange white space characters can cause problems on
    some OS languages
    (https://chocolatey.org/packages/visualstudio2017community#comment-3304858760).

--- visualstudio2017community/tools/ChocolateyInstall.ps1
+++ visualstudio2017community/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 Install-VisualStudio `
-    -PackageName 'visualstudio2017community' `
-    -ApplicationName 'Microsoft Visual Studio Community 2017' `
-    -Url 'https://download.microsoft.com/download/5/2/1/521C52A7-EFCC-46CE-A890-4F317732ABA3/vs_Community.exe' `
-    -Checksum 'A964FA078C0FA9115001302FF80D8119995814B58322646A46C4F3B894479471' `
-    -ChecksumType 'SHA256' `
-    -InstallerTechnology 'WillowVS2017OrLater' `
-    -ProgramsAndFeaturesDisplayName 'Microsoft Visual Studio 2017'
+    -PackageName 'visualstudio2017community' `
+    -ApplicationName 'Microsoft Visual Studio Community 2017' `
+    -Url 'https://download.microsoft.com/download/5/2/1/521C52A7-EFCC-46CE-A890-4F317732ABA3/vs_Community.exe' `
+    -Checksum 'A964FA078C0FA9115001302FF80D8119995814B58322646A46C4F3B894479471' `
+    -ChecksumType 'SHA256' `
+    -InstallerTechnology 'WillowVS2017OrLater' `
+    -ProgramsAndFeaturesDisplayName 'Microsoft Visual Studio 2017'
--- visualstudio2017professional/tools/ChocolateyInstall.ps1
+++ visualstudio2017professional/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 Install-VisualStudio `
-    -PackageName 'visualstudio2017professional' `
-    -ApplicationName 'Microsoft Visual Studio Professional 2017' `
-    -Url 'https://download.microsoft.com/download/C/5/0/C5092724-4FFE-4DD3-9EE7-0AE31BF17620/vs_Professional.exe' `
-    -Checksum 'EB4FC0A0C9C55AFDF86CF4D53625A41F554A4A49A7EDCAC6C9CFDFDC34FE1722' `
-    -ChecksumType 'SHA256' `
-    -InstallerTechnology 'WillowVS2017OrLater' `
-    -ProgramsAndFeaturesDisplayName 'Microsoft Visual Studio 2017'
+    -PackageName 'visualstudio2017professional' `
+    -ApplicationName 'Microsoft Visual Studio Professional 2017' `
+    -Url 'https://download.microsoft.com/download/C/5/0/C5092724-4FFE-4DD3-9EE7-0AE31BF17620/vs_Professional.exe' `
+    -Checksum 'EB4FC0A0C9C55AFDF86CF4D53625A41F554A4A49A7EDCAC6C9CFDFDC34FE1722' `
+    -ChecksumType 'SHA256' `
+    -InstallerTechnology 'WillowVS2017OrLater' `
+    -ProgramsAndFeaturesDisplayName 'Microsoft Visual Studio 2017'
