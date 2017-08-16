442347c0d13f8a492baeb1deeb1fd9683eeaea93
Chris Patterson <chrispat@microsoft.com>
2017-08-16 08:47:21 -0400

    Adding Visual Studio Component Functions

---
+++ chocolatey-visualstudio.extension/extensions/Remove-VisualStudioComponent.ps1
@@ -0,0 +1,21 @@
+function Remove-VisualStudioComponent
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)] [string] $PackageName,
+        [Parameter(Mandatory = $true)] [string] $Component,
+        [Parameter(Mandatory = $true)] [string] $VisualStudioYear,
+        [Parameter(Mandatory = $true)] [string[]] $ApplicableProducts
+    )
+    if ($Env:ChocolateyPackageDebug -ne $null)
+    {
+        $VerbosePreference = 'Continue'
+        $DebugPreference = 'Continue'
+        Write-Warning "VerbosePreference and DebugPreference set to Continue due to the presence of ChocolateyPackageDebug environment variable"
+    }
+
+    Write-Debug "Running 'Remove-VisualStudioComponent' with PackageName:'$PackageName' Component:'$Component' VisualStudioYear:'$VisualStudioYear'";
+    $argumentList = @('remove', "$Component")
+
+    Start-VisualStudioModifyOperation -PackageName $PackageName -ArgumentList $argumentList -VisualStudioYear $VisualStudioYear -ApplicableProducts $ApplicableProducts -OperationTexts @('uninstalled', 'uninstalling', 'uninstallation')
+}
