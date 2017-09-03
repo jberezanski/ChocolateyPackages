36d7285e7fba270c2f4098c3b0d5c6d32f3368de
Jakub Bereżański <kuba@berezanscy.pl>
2017-09-03 19:25:16 +0200

    improve VS 2017 tools
    - parse urls of more tools (Remote Tools, Perf Tools, IntelliTrace
      Collector)
    - download VS product bootstrappers using convention-based urls for
      improved reliability
    - print tool versions before building product layouts, so updating tool
      packages can be done quickly

--- tools/VS2017/Get-VS2017PackageUpdateData.ps1
+++ tools/VS2017/Get-VS2017PackageUpdateData.ps1
@@ -5,8 +5,9 @@ Set-StrictMode -Version 5
 Push-Location -Path $PSScriptRoot
 try
 {
-    .\Get-VS2017InstallerUrls.ps1 | .\Get-VS2017Installers.ps1 -OutVariable inst | ? Path -notlike '*vc_redist*' -PipelineVariable x | .\Invoke-VS2017LayoutCreation.ps1 | .\Get-VS2017BuildVersion.ps1 | % { [pscustomobject]@{Url = $x.Url; SHA256 = (Get-FileHash -Path $x.Path).Hash; BuildVersion = $_.BuildVersion } }
-    $inst | ? Path -like '*vc_redist*' -PipelineVariable x | % { [pscustomobject]@{Url = $x.Url; SHA256 = (Get-FileHash -Path $x.Path).Hash; BuildVersion = (Get-Item -Path $_.Path).VersionInfo.ProductVersion } }
+    $inst = .\Get-VS2017InstallerUrls.ps1 | .\Get-VS2017Installers.ps1
+    $inst | ? Path -cnotmatch 'vs_[A-Z]' -PipelineVariable x | % { [pscustomobject]@{Url = $x.Url; FileName = (Split-Path -Leaf -Path $x.Path); SHA256 = (Get-FileHash -Path $x.Path).Hash; BuildVersion = (Get-Item -Path $_.Path).VersionInfo.ProductVersion } }
+    $inst | ? Path -cmatch 'vs_[A-Z]' -PipelineVariable x | .\Invoke-VS2017LayoutCreation.ps1 | .\Get-VS2017BuildVersion.ps1 | % { [pscustomobject]@{Url = $x.Url; FileName = (Split-Path -Leaf -Path $x.Path); SHA256 = (Get-FileHash -Path $x.Path).Hash; BuildVersion = $_.BuildVersion } }
 }
 finally
 {
