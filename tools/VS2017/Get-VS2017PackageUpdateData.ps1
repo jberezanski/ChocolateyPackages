2629fbc41127b43317eec8b85daf9dbdb46eaa08
Jakub Bereżański <kuba@berezanscy.pl>
2017-12-12 21:27:25 +0100

    Get-VS2017PackageUpdateData: allow skipping layout creation

--- tools/VS2017/Get-VS2017PackageUpdateData.ps1
+++ tools/VS2017/Get-VS2017PackageUpdateData.ps1
@@ -1,5 +1,8 @@
 [CmdletBinding()]
-Param ()
+Param
+(
+    [switch] $SkipLayout
+)
 #Requires -Version 5
 Set-StrictMode -Version 5
 Push-Location -Path $PSScriptRoot
@@ -7,7 +10,10 @@ try
 {
     $inst = .\Get-VS2017InstallerUrls.ps1 | .\Get-VS2017Installers.ps1
     $inst | ? Path -cnotmatch 'vs_[A-Z]' -PipelineVariable x | % { [pscustomobject]@{Url = $x.Url; FileName = (Split-Path -Leaf -Path $x.Path); SHA256 = (Get-FileHash -Path $x.Path).Hash; BuildVersion = (Get-Item -Path $_.Path).VersionInfo.ProductVersion } }
-    $inst | ? Path -cmatch 'vs_[A-Z]' -PipelineVariable x | .\Invoke-VS2017LayoutCreation.ps1 | .\Get-VS2017BuildVersion.ps1 | % { [pscustomobject]@{Url = $x.Url; FileName = (Split-Path -Leaf -Path $x.Path); SHA256 = (Get-FileHash -Path $x.Path).Hash; BuildVersion = $_.BuildVersion } }
+    if (-not $SkipLayout)
+    {
+        $inst | ? Path -cmatch 'vs_[A-Z]' -PipelineVariable x | .\Invoke-VS2017LayoutCreation.ps1 | .\Get-VS2017BuildVersion.ps1 | % { [pscustomobject]@{Url = $x.Url; FileName = (Split-Path -Leaf -Path $x.Path); SHA256 = (Get-FileHash -Path $x.Path).Hash; BuildVersion = $_.BuildVersion } }
+    }
 }
 finally
 {
