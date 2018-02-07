7e5fae089138ac6aa83fc984faca4ec9e0c507e0
Jakub Bereżański <kuba@berezanscy.pl>
2018-02-07 20:50:22 +0100

    import NugetPackageManagerForVisualStudio2013 version 2.8.50313.4600

---
+++ NugetPackageManagerForVisualStudio2013/tools/chocolateyInstall.ps1
@@ -0,0 +1,3 @@
+$name = "NugetPackageManagerForVisualStudio2013"
+$url  = "http://visualstudiogallery.msdn.microsoft.com/4ec1526c-4a8c-4a84-b702-b21a8f5293ca/file/105933/6/NuGet.Tools.2013.vsix"
+Install-ChocolateyVsixPackage $name $url
