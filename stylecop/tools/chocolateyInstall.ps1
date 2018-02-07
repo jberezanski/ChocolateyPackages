a88c0ab309eac8a888e7da663c44dbe71b19907e
Jakub Bereżański <kuba@berezanscy.pl>
2018-02-07 22:57:33 +0100

    update stylecop to 4.7.52

--- stylecop/tools/chocolateyInstall.ps1
+++ stylecop/tools/chocolateyInstall.ps1
@@ -2,5 +2,5 @@
   -PackageName 'stylecop' `
   -FileType 'msi' `
   -SilentArgs '/quiet' `
-  -File "$(Split-Path -Parent -Path $MyInvocation.MyCommand.Definition)\StyleCop-4.7.49.0.msi" `
+  -File "$(Split-Path -Parent -Path $MyInvocation.MyCommand.Definition)\StyleCop-4.7.52.0.msi" `
   -ValidExitCodes @(0, 3010)
