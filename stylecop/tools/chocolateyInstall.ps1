a5cfa5fc62194d7c6560765bf2b4d38184488c9e
Jakub Bereżański <kuba@berezanscy.pl>
2018-02-07 22:34:07 +0100

    convert stylecop to embedded package

--- stylecop/tools/chocolateyInstall.ps1
+++ stylecop/tools/chocolateyInstall.ps1
@@ -1,1 +1,6 @@
-﻿Install-ChocolateyPackage 'stylecop' 'msi' '/quiet' 'http://download.codeplex.com/Download?ProjectName=stylecop&DownloadId=323236&FileTime=129725389339900000&Build=18394'
+﻿Install-ChocolateyInstallPackage `
+  -PackageName 'stylecop' `
+  -FileType 'msi' `
+  -SilentArgs '/quiet' `
+  -File "$(Split-Path -Parent -Path $MyInvocation.MyCommand.Definition)\StyleCop-4.7.49.0.msi" `
+  -ValidExitCodes @(0, 3010)
