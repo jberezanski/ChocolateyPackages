1a860efa5e61a865c21f67a45f60ed002cf6bc05
Jakub Bereżański <kuba@berezanscy.pl>
2018-02-08 09:46:24 +0100

    NugetPackageManagerForVisualStudio2013: update metadata and match structure with visualstudio2015-nugetpackagemanager

--- NugetPackageManagerForVisualStudio2013/tools/chocolateyInstall.ps1
+++ NugetPackageManagerForVisualStudio2013/tools/chocolateyInstall.ps1
@@ -1,4 +1,9 @@
-$name = "NugetPackageManagerForVisualStudio2013"
-$url  = 'https://nugetteam.gallerycdn.vsassets.io/extensions/nugetteam/nugetpackagemanagerforvisualstudio2013/2.12.0.817/1488284100949/105933/11/NuGet.Tools.vsix'
-$sha256 = '46F0747DC5E122208A14DA88AC7D3CF87EE4153BC9E643CA1D94D7F6274BF600'
-Install-ChocolateyVsixPackage -PackageName $name -VsVersion 12 -VsixUrl $url -Checksum $sha256 -ChecksumType SHA256
+$packageArgs = @{
+  PackageName   = 'NugetPackageManagerForVisualStudio2013'
+  VsVersion     = 12
+  VsixUrl       = 'https://nugetteam.gallerycdn.vsassets.io/extensions/nugetteam/nugetpackagemanagerforvisualstudio2013/2.12.0.817/1488284100949/105933/11/NuGet.Tools.vsix'
+  Checksum      = '46F0747DC5E122208A14DA88AC7D3CF87EE4153BC9E643CA1D94D7F6274BF600'
+  ChecksumType  = 'sha256'
+}
+
+Install-ChocolateyVsixPackage @packageArgs
