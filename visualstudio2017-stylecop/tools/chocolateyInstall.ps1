4cdf1663f4698e06a14d4807ab74f51cff53de08
Jakub Bereżański <kuba@berezanscy.pl>
2018-02-08 21:54:19 +0100

    add visualstudio2017-stylecop

---
+++ visualstudio2017-stylecop/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  PackageName   = 'visualstudio2017-stylecop'
+  VsVersion     = 15
+  VsixUrl       = 'https://chrisdahlberg.gallerycdn.vsassets.io/extensions/chrisdahlberg/stylecop/5.0.6419.0/1501345807969/231103/4/StyleCop.vsix'
+  Checksum      = '212738A32AB1AF0EDE8C42F1B574EE6A67A88E69AF7EFD744E48B9AD05EE84A5'
+  ChecksumType  = 'sha256'
+}
+
+Install-ChocolateyVsixPackage @packageArgs
