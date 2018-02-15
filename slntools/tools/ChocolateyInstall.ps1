b32e2667146f94f11fbcccdcd9d34ed55a150e7c
Jakub Bereżański <kuba@berezanscy.pl>
2018-02-15 20:36:10 +0100

    add slntools

---
+++ slntools/tools/ChocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'slntools'
+$arguments = @{
+    packageName = $packageName
+    fileType = 'msi'
+    silentArgs = ('/quiet /norestart /log "{0}\{1}-{2:yyyyMMddHHmmss}.log"' -f $Env:TEMP, $packageName, (Get-Date))
+    validExitCodes = @(
+        0, # success
+        3010 # success, restart required
+    )
+    url = 'https://github.com/mtherien/slntools/releases/download/1.1.3/SLNTools_v.1.1.3.msi'
+    checksum = 'CA800CB26261295E6301164C72BA69D4853589E3430D508928541247EAA4D2C3'
+    checksumType = 'sha256'
+}
+
+Install-ChocolateyPackage @arguments
