fe97df6d827add88c30772ce3a7e78f8e3b37d1b
Jakub Bereżański <kuba@berezanscy.pl>
2018-02-16 22:43:43 +0100

    dotnetfx: include .NET version in log file name

--- dotnetfx/tools/ChocolateyInstall.ps1
+++ dotnetfx/tools/ChocolateyInstall.ps1
@@ -1,14 +1,15 @@
 ﻿. (Join-Path -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition) -ChildPath 'helpers.ps1')
 
-$productNameWithVersion = 'Microsoft .NET Framework 4.7.1'
-$release = 461308
 $packageName = 'dotnetfx'
+$release = 461308
+$version = '4.7.1'
+$productNameWithVersion = "Microsoft .NET Framework $version"
 $url = 'https://download.microsoft.com/download/9/E/6/9E63300C-0941-4B45-A0EC-0008F96DD480/NDP471-KB4033342-x86-x64-AllOS-ENU.exe'
 $checksum = '63DC850DF091F3F137B5D4392F47917F847F8926DC8AF1DA9BFBA6422E495805'
 $checksumType = 'sha256'
 $arguments = @{
     packageName = $packageName
-    silentArgs = ('/Quiet /NoRestart /Log "{0}\{1}_{2:yyyyMMddHHmmss}.log"' -f (Get-SafeLogPath), $packageName, (Get-Date))
+    silentArgs = ('/Quiet /NoRestart /Log "{0}\{1}_{2}_{3:yyyyMMddHHmmss}.log"' -f (Get-SafeLogPath), $packageName, $version, (Get-Date))
     validExitCodes = @(
         0, # success
         3010 # success, restart required
