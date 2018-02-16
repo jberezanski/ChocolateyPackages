2796615ed4787201917600ed537896834012429b
Jakub Bereżański <kuba@berezanscy.pl>
2018-02-16 22:35:26 +0100

    dotnetfx: ensure log path does not point to a network share
    This works around
    https://github.com/jberezanski/ChocolateyPackages/issues/15.

--- dotnetfx/tools/ChocolateyInstall.ps1
+++ dotnetfx/tools/ChocolateyInstall.ps1
@@ -1,4 +1,6 @@
-﻿$productNameWithVersion = 'Microsoft .NET Framework 4.7.1'
+﻿. (Join-Path -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition) -ChildPath 'helpers.ps1')
+
+$productNameWithVersion = 'Microsoft .NET Framework 4.7.1'
 $release = 461308
 $packageName = 'dotnetfx'
 $url = 'https://download.microsoft.com/download/9/E/6/9E63300C-0941-4B45-A0EC-0008F96DD480/NDP471-KB4033342-x86-x64-AllOS-ENU.exe'
@@ -6,7 +8,7 @@ $checksum = '63DC850DF091F3F137B5D4392F47917F847F8926DC8AF1DA9BFBA6422E495805'
 $checksumType = 'sha256'
 $arguments = @{
     packageName = $packageName
-    silentArgs = ('/Quiet /NoRestart /Log "{0}\{1}_{2:yyyyMMddHHmmss}.log"' -f $Env:TEMP, $packageName, (Get-Date))
+    silentArgs = ('/Quiet /NoRestart /Log "{0}\{1}_{2:yyyyMMddHHmmss}.log"' -f (Get-SafeLogPath), $packageName, (Get-Date))
     validExitCodes = @(
         0, # success
         3010 # success, restart required
@@ -17,12 +19,6 @@ $arguments = @{
     url64 = $url
     checksum64 = $checksum
     checksumType64 = $checksumType
-}
-
-function Test-Installed($Release)
-{
-    $props = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full' -Name Release -ErrorAction SilentlyContinue
-    return $props -ne $null -and $props.Release -ge $Release
 }
 
 if (Test-Installed -Release $release) {
---
+++ dotnetfx/tools/helpers.ps1
@@ -0,0 +1,66 @@
+﻿$scriptDirectory = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+
+function Get-SafeLogPath
+{
+    $logPath = $Env:TEMP
+    if ($logPath -like '\\*')
+    {
+        # .NET installer does not like logging to a network share (https://github.com/jberezanski/ChocolateyPackages/issues/15)
+        $candidates = @("$Env:LOCALAPPDATA\Temp\chocolatey", "$Env:LOCALAPPDATA\Temp", "$Env:USERPROFILE\AppData\Local\Temp\chocolatey", "$Env:USERPROFILE\AppData\Local\Temp", "$Env:SystemRoot\TEMP", $scriptDirectory)
+        foreach ($candidate in $candidates)
+        {
+            if ((Test-Path -Path $candidate) -and $candidate -notlike '\\*')
+            {
+                Write-Verbose "Using '$candidate' as log path because `$Env:TEMP points to a network share, which may cause the installation to fail"
+                $logPath = $candidate
+                break
+            }
+        }
+    }
+
+    return $logPath
+}
+
+function Test-Installed($Release)
+{
+    $props = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full' -Name Release -ErrorAction SilentlyContinue
+    return $props -ne $null -and $props.Release -ge $Release
+}
+
+function Install-ChocolateyInstallPackage
+{
+    Param
+    (
+        [parameter(Mandatory=$true, Position=0)][string] $packageName,
+        [parameter(Mandatory=$false, Position=1)]
+        [alias("installerType","installType")][string] $fileType = 'exe',
+        [parameter(Mandatory=$false, Position=2)][string[]] $silentArgs = '',
+        [alias("fileFullPath")][parameter(Mandatory=$false, Position=3)][string] $file,
+        [alias("fileFullPath64")][parameter(Mandatory=$false)][string] $file64,
+        [parameter(Mandatory=$false)] $validExitCodes = @(0),
+        [parameter(Mandatory=$false)]
+        [alias("useOnlyPackageSilentArgs")][switch] $useOnlyPackageSilentArguments = $false,
+        [parameter(ValueFromRemainingArguments = $true)][Object[]] $ignoredArguments
+    )
+
+    $oldTemp = $Env:TEMP
+    $safeLogPath = Get-SafeLogPath
+    if ($Env:TEMP -ne $safeLogPath)
+    {
+        Write-Debug "Changing `$Env:TEMP from '$oldTemp' to '$safeLogPath'"
+        $Env:TEMP = $safeLogPath
+    }
+
+    try
+    {
+        chocolateyInstaller\Install-ChocolateyInstallPackage @PSBoundParameters
+    }
+    finally
+    {
+        if ($Env:TEMP -ne $oldTemp)
+        {
+            Write-Debug "Changing `$Env:TEMP back to '$oldTemp'"
+            $Env:TEMP = $oldTemp
+        }
+    }
+}
