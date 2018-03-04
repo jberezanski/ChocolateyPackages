caddf20ba5783dd31042152b562e29f166a8940c
Jakub Bereżański <kuba@berezanscy.pl>
2018-03-05 00:05:17 +0100

    azure-powershell: skip installation if same or newer version is found

--- azure-powershell/tools/ChocolateyInstall.ps1
+++ azure-powershell/tools/ChocolateyInstall.ps1
@@ -1,7 +1,36 @@
 ﻿$packageName = 'azure-powershell'
+$moduleVersion = [version]'5.0.0'
 $url = 'https://github.com/Azure/azure-powershell/releases/download/v5.0.0-November2017/azure-powershell.5.0.0.msi'
 $checksum = '50C1A943739D2043C3D74B601908B6E21CEFF90CAD6E30D86F931016FA6A2F5D'
 $checksumType = 'sha256'
+
+if (-not (Test-Path -Path "HKLM:\Software\Microsoft\PowerShell\3") -or $PSVersionTable -eq $null -or $PSVersionTable.PSVersion -lt [version]'3.0')
+{
+    throw "Azure PowerShell requires PowerShell version 3 or greater. You can install it using the 'powershell' package."
+}
+else
+{
+    Write-Debug 'Running on PowerShell 3 or later'
+}
+
+$newestModule = Get-Module -Name AzureRM.* -ListAvailable | Sort-Object -Property Version -Descending | Select-Object -First 1
+if ($newestModule -ne $null -and $newestModule.Version -ge $moduleVersion)
+{
+    Write-Host "Azure PowerShell version $moduleVersion or later is already installed (detected module $($newestModule.Name) version $($newestModule.Version))."
+    return
+}
+else
+{
+    if ($newestModule -ne $null)
+    {
+        Write-Debug "Azure PowerShell version $moduleVersion or later is not installed (detected module $($newestModule.Name) version $($newestModule.Version))."
+    }
+    else
+    {
+        Write-Debug "Azure PowerShell version $moduleVersion or later is not installed (no modules detected)."
+    }
+}
+
 $arguments = @{
     packageName = $packageName
     installerType = 'msi'
@@ -16,11 +45,6 @@ $arguments = @{
     url64 = $url
     checksum64 = $checksum
     checksumType64 = $checksumType
-}
-
-if (-not (Test-Path -Path "HKLM:\Software\Microsoft\PowerShell\3"))
-{
-    throw "Azure PowerShell requires PowerShell version 3 or greater. You can install it using the 'powershell' package."
 }
 
 Install-ChocolateyPackage @arguments
