b3a8fe18fcd619ad6b7a30369f0c5ba591c37f58
Jakub Bereżański <kuba@berezanscy.pl>
2018-03-17 20:47:22 +0100

    netfx-4.7.2-devpack: apply TEMP fix from dotnetfx

--- netfx-4.7.2-devpack/tools/ChocolateyInstall.ps1
+++ netfx-4.7.2-devpack/tools/ChocolateyInstall.ps1
@@ -1,14 +1,17 @@
-﻿$packageName = 'netfx-4.7.2-devpack'
+﻿. (Join-Path -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition) -ChildPath 'helpers.ps1')
+
+$packageName = 'netfx-4.7.2-devpack'
+$version = '4.7.2'
+$productNameWithVersion = "Microsoft .NET Framework $version Developer Pack early access build 3056"
 $url = 'https://download.microsoft.com/download/8/0/2/802C4F8E-954E-48E9-B621-39D966110D4F/NDP472-DevPack-ENU.exe'
 $checksum = '4238ABA129D2B77976C93D51252E5CF06B627B6E1685410DFC9CF35E971B4239'
 $checksumType = 'sha256'
-$arguments = @{
+
+$originalFileName = Split-Path -Leaf -Path ([uri]$url).LocalPath
+$downloadFilePath = Get-DefaultChocolateyLocalFilePath -OriginalFileName $originalFileName
+$downloadArguments = @{
     packageName = $packageName
-    silentArgs = "/Quiet /NoRestart /Log ""${Env:TEMP}\${packageName}.log"""
-    validExitCodes = @(
-        0, # success
-        3010 # success, restart required
-    )
+    fileFullPath = $downloadFilePath
     url = $url
     checksum = $checksum
     checksumType = $checksumType
@@ -17,4 +20,29 @@ $arguments = @{
     checksumType64 = $checksumType
 }
 
-Install-ChocolateyPackage @arguments
+$filePath = Get-ChocolateyWebFile @downloadArguments
+
+$safeLogPath = Get-SafeLogPath
+$installerExeArguments = @{
+    packageName = $packageName
+    file = $downloadFilePath
+    silentArgs = ('/Quiet /NoRestart /Log "{0}\{1}_{2}_{3:yyyyMMddHHmmss}.log"' -f $safeLogPath, $packageName, $version, (Get-Date))
+    validExitCodes = @(
+        0, # success
+        3010 # success, restart required
+    )
+}
+
+Invoke-CommandWithTempPath -TempPath $safeLogPath -ScriptBlock { Install-ChocolateyInstallPackage @installerExeArguments }
+
+if ($Env:ChocolateyExitCode -eq '3010')
+{
+    Write-Warning "A restart is required to finalize $productNameWithVersion installation."
+}
+else
+{
+    if ($Env:ChocolateyExitCode -eq $null)
+    {
+        Write-Host "A restart may be required to finalize $productNameWithVersion installation."
+    }
+}
