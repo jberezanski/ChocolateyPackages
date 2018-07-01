900a280f627b3aef032d2f756f3e02a2efd04c7b
Jakub Bereżański <kuba@berezanscy.pl>
2018-07-02 01:29:21 +0200

    fix multiple issues in Get-VS2017PackageUpdateData

--- tools/VS2017/Get-VS2017PackageUpdateData.ps1
+++ tools/VS2017/Get-VS2017PackageUpdateData.ps1
@@ -10,26 +10,29 @@ Set-StrictMode -Version 5
 Push-Location -Path $PSScriptRoot
 try
 {
+    $mainProducts = @('BuildTools','Community','Enterprise','FeedbackClient','Professional','TeamExplorer','TestAgent','TestController','TestProfessional')
     $inst = .\Get-VS2017InstallerUrls.ps1 | .\Get-VS2017Installers.ps1 -DestinationPath $DestinationPath
-    $outputPath = $inst | select -First 1 -ExpandProperty Path | Split-Path -Parent
-    $inst | ? Path -cnotmatch 'vs_[A-Z]' -PipelineVariable x | % { [pscustomobject]@{Url = $x.Url; FileName = (Split-Path -Leaf -Path $x.Path); SHA256 = (Get-FileHash -Path $x.Path).Hash; BuildVersion = (Get-Item -Path $_.Path).VersionInfo.ProductVersion } } -OutVariable auxData
+    $outputPath = $inst | Select-Object -First 1 -ExpandProperty Path | Split-Path -Parent
+    $mainInst = $inst | Where-Object Url -imatch ('vs_({0}).exe$' -f (($mainProducts | ForEach-Object { "($_)" }) -join '|'))
+    $auxInst = $inst | Where-Object { $mainInst -notcontains $_ }
+    $auxInst | ForEach-Object { [pscustomobject]@{Url = $_.Url; FileName = (Split-Path -Leaf -Path $_.Path); SHA256 = (Get-FileHash -Path $_.Path).Hash; BuildVersion = (Get-Item -Path $_.Path).VersionInfo.ProductVersion } } -OutVariable auxData
     $auxData | Export-Clixml -Path "$outputPath\AuxData.clixml"
     # compute and save hashes, so that the important info remains even if layout fails
-    $inst | ? Path -cmatch 'vs_[A-Z]' -PipelineVariable x | % { [pscustomobject]@{Url = $x.Url; FileName = (Split-Path -Leaf -Path $x.Path); SHA256 = (Get-FileHash -Path $x.Path).Hash; BuildVersion = [version]'0.0.0' } } -OutVariable mainData | Out-Null
+    $mainInst | ForEach-Object { [pscustomobject]@{Url = $_.Url; FileName = (Split-Path -Leaf -Path $_.Path); SHA256 = (Get-FileHash -Path $_.Path).Hash; BuildVersion = [version]'0.0.0' } } -OutVariable mainData | Out-Null
     $mainData | Export-Clixml -Path "$outputPath\MainDataWithoutVersion.clixml"
     if (-not $SkipLayout)
     {
         if ($AllLayouts)
         {
-            $todo = $inst
+            $todo = $mainInst
         }
         else
         {
-            $todo = $inst | Where-Object Path -like '*vs_FeedbackClient*'
+            $todo = $mainInst | Where-Object Url -like '*vs_FeedbackClient*'
         }
 
         # prepare and save mainData again, this time with version info
-        $todo | ? Path -cmatch 'vs_[A-Z]' -PipelineVariable x | .\Invoke-VS2017LayoutCreation.ps1 | .\Get-VS2017BuildVersion.ps1 | % { [pscustomobject]@{Url = $x.Url; FileName = (Split-Path -Leaf -Path $x.Path); SHA256 = (Get-FileHash -Path $x.Path).Hash; BuildVersion = $_.BuildVersion } } -OutVariable mainData
+        $todo | ForEach-Object { $_ } -PipelineVariable x | .\Invoke-VS2017LayoutCreation.ps1 | .\Get-VS2017BuildVersion.ps1 | % { [pscustomobject]@{Url = $x.Url; FileName = (Split-Path -Leaf -Path $x.Path); SHA256 = (Get-FileHash -Path $x.Path).Hash; BuildVersion = $_.BuildVersion } } -OutVariable mainData
         $mainData | Export-Clixml -Path "$outputPath\MainData.clixml"
     }
     else
