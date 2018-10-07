8d775cd1f82ca5944274dcceb886add1e48cd6b0
Jakub Bereżański <kuba@berezanscy.pl>
2018-10-07 21:32:29 +0200

    update .NET 4.8 preview to build 3646 and add Developer Pack

--- netfx-4.8-devpack/tools/ChocolateyInstall.ps1
+++ netfx-4.8-devpack/tools/ChocolateyInstall.ps1
@@ -1,17 +1,11 @@
 ﻿. (Join-Path -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition) -ChildPath 'helpers.ps1')
 
-$packageName = 'dotnetfx'
-$release = 999999
-$version = '4.8.0'
-$productNameWithVersion = "Microsoft .NET Framework $version early access build 3646"
-$url = 'https://download.microsoft.com/download/6/5/7/6577634A-8D5D-4558-BA22-A81CC6D5BB06/NDP48-Preview-x86-x64-AllOS-ENU.exe'
-$checksum = '5D6F819B273D097586286284FCB654B3400A41C446062CF3E426C1FB92F9119A'
+$packageName = 'netfx-4.8-devpack'
+$version = '4.8'
+$productNameWithVersion = "Microsoft .NET Framework $version Developer Pack early access build 3646"
+$url = 'https://download.microsoft.com/download/6/5/7/6577634A-8D5D-4558-BA22-A81CC6D5BB06/NDP48-DevPack-ENU.exe'
+$checksum = '33DA4EFDCE614B0E8B600307B7F3782CAAA4B4F08745BA957F46618227B5EB73'
 $checksumType = 'sha256'
-
-if (Test-Installed -Release $release) {
-    Write-Host "$productNameWithVersion or later is already installed."
-    return
-}
 
 $originalFileName = Split-Path -Leaf -Path ([uri]$url).LocalPath
 $downloadFilePath = Get-DefaultChocolateyLocalFilePath -OriginalFileName $originalFileName
@@ -48,7 +42,7 @@ if ($Env:ChocolateyExitCode -eq '3010')
 else
 {
     if ($Env:ChocolateyExitCode -eq $null)
-    { 
+    {
         Write-Host "A restart may be required to finalize $productNameWithVersion installation."
     }
 }
