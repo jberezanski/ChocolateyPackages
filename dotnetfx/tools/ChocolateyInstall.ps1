a9dea7f042bfcb4eea859f7be4c06673a1392c6a
Jakub Bereżański <kuba@berezanscy.pl>
2018-10-07 21:50:39 +0200

    .NET 4.8: use offical version name

--- dotnetfx/tools/ChocolateyInstall.ps1
+++ dotnetfx/tools/ChocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'dotnetfx'
 $release = 999999
-$version = '4.8.0'
+$version = '4.8'
 $productNameWithVersion = "Microsoft .NET Framework $version early access build 3646"
 $url = 'https://download.microsoft.com/download/6/5/7/6577634A-8D5D-4558-BA22-A81CC6D5BB06/NDP48-Preview-x86-x64-AllOS-ENU.exe'
 $checksum = '5D6F819B273D097586286284FCB654B3400A41C446062CF3E426C1FB92F9119A'
