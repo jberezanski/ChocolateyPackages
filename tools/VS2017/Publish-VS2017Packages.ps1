0755d1fdaea837e8ec9f870eb3672d69d3d34405
Jakub Bereżański <kuba@berezanscy.pl>
2018-11-17 01:58:55 +0100

    add script for building and publishing VS 2017 product packages

---
+++ tools/VS2017/Publish-VS2017Packages.ps1
@@ -0,0 +1,50 @@
+﻿[CmdletBinding(SupportsShouldProcess = $true)]
+Param
+(
+)
+
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version Latest
+
+Push-Location -Path "$PSScriptRoot\..\.."
+try
+{
+    $todo = New-Object Collections.ArrayList
+    Get-ChildItem | Where-Object Name -match '^visualstudio2017[a-z]' | ForEach-Object {
+        $n = $_.Name
+        $v = ([xml](Get-Content -Path ".\$n\$n.nuspec")).package.metadata.version
+        $p = "$PWD\output\$n.$v.nupkg"
+        if ($PSCmdlet.ShouldProcess("Package: $n", 'Build'))
+        {
+            .\build.cmd $n
+            Resolve-Path -Path $p | Out-Null
+        }
+
+        $t = "$n-$v"
+        if ($PSCmdlet.ShouldProcess("Tag: $t", 'Create'))
+        {
+            git tag -am "publish $n $v" $t
+        }
+
+        $o = [pscustomobject]@{ Nupkg = $p; Tag = $t }
+        [void]$todo.Add($o)
+    }
+
+    $todo | ForEach-Object {
+        $p = $_.Nupkg
+        if ($PSCmdlet.ShouldProcess($p, 'Push'))
+        {
+            cpush $p
+        }
+
+        $t = $_.Tag
+        if ($PSCmdlet.ShouldProcess("Tag: $t", 'Push'))
+        {
+            git push origin $t
+        }
+    }
+}
+finally
+{
+    Pop-Location
+}
