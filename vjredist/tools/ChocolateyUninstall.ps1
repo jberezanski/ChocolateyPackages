0d274804db6caf86fc41ec8d7a0b2ca6bb6ad78c
Jakub Bereżański <kuba@berezanscy.pl>
2019-02-26 15:31:04 +0100

    vjredist: add uninstall support

---
+++ vjredist/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,14 @@
+$installExePath = Join-Path -Path $Env:SystemRoot 'Microsoft.NET\Framework\v2.0.50727\Microsoft Visual J# 2.0 Redistributable Package\install.exe'
+if (Test-Path -Path $installExePath)
+{
+    Write-Verbose "Uninstaller file exists ($installExePath)"
+    Start-ChocolateyProcessAsAdmin `
+        -ExeToRun $installExePath `
+        -Statements '/qu' `
+        -ValidExitCodes @(0, 3010) `
+        -WorkingDirectory "${Env:SystemDrive}\"
+}
+else
+{
+    Write-Verbose "Uninstaller file does not exist, software has probably been uninstalled already ($installExePath)"
+}
