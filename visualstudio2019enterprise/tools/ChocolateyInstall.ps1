af64018e05bb7eaeadde0bc7c8832899391625c3
Jakub Bereżański <kuba@berezanscy.pl>
2019-03-09 20:16:22 +0100

    add visualstudio2019enterprise

---
+++ visualstudio2019enterprise/tools/ChocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿Install-VisualStudio `
+    -PackageName 'visualstudio2019enterprise' `
+    -ApplicationName 'Microsoft Visual Studio Enterprise 2019' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/3403e8ff-4696-451b-b625-65bb250c902a/10f2fbc23868d5b3fb53ba81309de009/vs_enterprise.exe' <# https://aka.ms/vs/16/release/vs_enterprise.exe #> `
+    -Checksum 'D94BD3EDA1185DB0C924F5FB03425137B15D48290CFB605E1DA582E6C92756E5' `
+    -ChecksumType 'SHA256' `
+    -InstallerTechnology 'WillowVS2017OrLater' `
+    -Product 'Enterprise' `
+    -VisualStudioYear '2019' `
+    -Preview $false
---
+++ visualstudio2019enterprise/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,5 @@
+﻿Remove-VisualStudioProduct `
+    -PackageName 'visualstudio2019enterprise' `
+    -Product 'Enterprise' `
+    -VisualStudioYear '2019' `
+    -Preview $false
