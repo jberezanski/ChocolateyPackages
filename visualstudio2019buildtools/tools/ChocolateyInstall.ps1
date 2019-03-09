d00e1bf8be2e1106fe7a0ad15fff6d68762a05e6
Jakub Bereżański <kuba@berezanscy.pl>
2019-03-09 22:14:50 +0100

    add visualstudio2019buildtools

---
+++ visualstudio2019buildtools/tools/ChocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿Install-VisualStudio `
+    -PackageName 'visualstudio2019buildtools' `
+    -ApplicationName 'Microsoft Visual Studio Build Tools 2019' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/564141f6-3486-4e8f-be43-655c09309eee/1263efda14ecd7ca5cf3f92e53e4d268/vs_buildtools.exe' <# https://aka.ms/vs/16/release/vs_buildtools.exe #> `
+    -Checksum 'C253A62D8BA0CF80EEC9450A2D27FC6FA8F084372ABDA12018094111EF0C7304' `
+    -ChecksumType 'SHA256' `
+    -InstallerTechnology 'WillowVS2017OrLater' `
+    -Product 'BuildTools' `
+    -VisualStudioYear '2019' `
+    -Preview $false
---
+++ visualstudio2019buildtools/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,5 @@
+﻿Remove-VisualStudioProduct `
+    -PackageName 'visualstudio2019buildtools' `
+    -Product 'BuildTools' `
+    -VisualStudioYear '2019' `
+    -Preview $false
