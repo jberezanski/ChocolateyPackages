1df4e9cbc51866208c09061e4059a470ce1fc12a
Christian Bumann <bch@bbtsoftware.ch>
2019-05-01 13:48:01 +0200

    Support the --installCatalogUri parameter
    https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=vs-2017#layout-options

--- chocolatey-visualstudio.extension/extensions/Get-VSComponentManifest.ps1
+++ chocolatey-visualstudio.extension/extensions/Get-VSComponentManifest.ps1
@@ -26,6 +26,37 @@ function Get-VSComponentManifest
         $ChannelManifest = Get-VSChannelManifest -PackageParameters $PackageParameters -ChannelReference $ChannelReference -LayoutPath $layoutPath -UseInstallChannelUri:$UseInstallChannelUri
     }
 
+    # https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=vs-2017#layout-options
+    # Advanced install options: Parameter --installCatalogUri <uri>
+    if ($packageParameters.ContainsKey('installCatalogUri'))
+    {
+        Write-Debug 'Obtaining the installCatalogUri from the Parameters'
+        $url = $packageParameters['installCatalogUri']
+
+        if ($url -eq $null)
+        {
+            Write-Verbose 'Unable to determine the catalog manifest url'
+            # If the installCatalogUri is null continue with the url from the ChannelManifest as the Microsoft documentation says.
+        }
+        else 
+        {
+            try
+            {
+                return Get-VSManifest -Description 'catalog manifest' -Url $url -LayoutFileName 'Catalog.json' -LayoutPath $layoutPath
+            }
+            catch
+            {
+                # The catalog manifest could not be downloaded from the installCatalogUri.
+                # Catch the error and continue with the url from the ChannelManifest as the Microsoft documentation says.
+            }
+        }
+
+        # Optional: The URI of the catalog manifest to use for the installation. If specified, the channel manager attempts to download
+        # the catalog manifest from this URI before using the URI in the install channel manifest. This parameter is used to support offline
+        # install, where the layout cache will be created with the product catalog already downloaded. This can be used for the install command;
+        # it is ignored for other commands.
+    }
+
     Write-Debug 'Parsing the channel manifest'
     $url, $checksum, $checksumType = Get-VSChannelManifestItemUrl -Manifest $ChannelManifest -ChannelItemType 'Manifest'
 
