c9757a4b8612b0853f2548ebcc8349a3e63c4b15
Jakub Bereżański <kuba@berezanscy.pl>
2019-08-21 15:24:14 +0200

    update dotnetfx, netfx-4.8 and netfx-4.8-devpack installer urls

--- netfx-4.8-devpack/tools/ChocolateyInstall.ps1
+++ netfx-4.8-devpack/tools/ChocolateyInstall.ps1
@@ -5,8 +5,8 @@ $arguments = @{
     PackageName = "netfx-$version-devpack"
     Version = $version
     ProductNameWithVersion = "Microsoft .NET Framework $version Developer Pack"
-    Url = 'https://download.visualstudio.microsoft.com/download/pr/7afca223-55d2-470a-8edc-6a1739ae3252/c8c829444416e811be84c5765ede6148/NDP48-DevPack-ENU.exe'
-    Checksum = '3AEB6E4B1D3D5D36689A50026231F725AB0BCC145E72A09B13394FD9C8480B7F'
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/014120d7-d689-4305-befd-3cb711108212/0307177e14752e359fde5423ab583e43/ndp48-devpack-enu.exe'
+    Checksum = '0A7AC4A9B44CED6BB7A0EBF3AD9BA29F60BD4D3BEB2047E19F4D8749DE61F5AC'
     ChecksumType = 'sha256'
 }
 
--- netfx-4.8/tools/ChocolateyInstall.ps1
+++ netfx-4.8/tools/ChocolateyInstall.ps1
@@ -6,8 +6,8 @@ $arguments = @{
     Release = 528040
     Version = $version
     ProductNameWithVersion = "Microsoft .NET Framework $version"
-    Url = 'https://download.visualstudio.microsoft.com/download/pr/7afca223-55d2-470a-8edc-6a1739ae3252/abd170b4b0ec15ad0222a809b761a036/NDP48-x86-x64-AllOS-ENU.exe'
-    Checksum = '95889D6DE3F2070C07790AD6CF2000D33D9A1BDFC6A381725AB82AB1C314FD53'
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/014120d7-d689-4305-befd-3cb711108212/0fd66638cde16859462a6243a4629a50/ndp48-x86-x64-allos-enu.exe'
+    Checksum = '9B1F71CD1B86BB6EE6303F7BE6FBBE71807A51BB913844C85FC235D5978F3A0F'
     ChecksumType = 'sha256'
 }
 
