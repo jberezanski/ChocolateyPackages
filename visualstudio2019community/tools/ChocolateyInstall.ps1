467ffc6d210f879110f45ec41d29f817ad0976b0
Sergio Haro <sharo@quickbase.com>
2020-01-17 14:47:52 -0800

    update vs19 community

--- visualstudio2019community/tools/ChocolateyInstall.ps1
+++ visualstudio2019community/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿Install-VisualStudio `
     -PackageName 'visualstudio2019community' `
     -ApplicationName 'Microsoft Visual Studio Community 2019' `
-    -Url 'https://download.visualstudio.microsoft.com/download/pr/f6473c9f-a5f6-4249-af28-c2fd14b6a0fb/5082309bc336fec6ee9ff09cd0bf9d755b85c49732cf5673c35e7778dc0e221a/vs_Community.exe' <# https://aka.ms/vs/16/release/vs_community.exe #> `
-    -Checksum '5082309BC336FEC6EE9FF09CD0BF9D755B85C49732CF5673C35E7778DC0E221A' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/b79d5d47-f792-4eec-adcf-2da2dfa3e3db/4961e5fa0e5d318394f1e3cb8314a8ebe24f3f414a966f0fe0e4b8719e1cf669/vs_Community.exe' <# https://aka.ms/vs/16/release/vs_community.exe #> `
+    -Checksum '4961E5FA0E5D318394F1E3CB8314A8EBE24F3F414A966F0FE0E4B8719E1CF669' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
     -Product 'Community' `
