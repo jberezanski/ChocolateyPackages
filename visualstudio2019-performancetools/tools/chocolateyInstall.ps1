6582853c6a3d5c98868f89b0a2b93acc1468c5e4
Jakub Bereżański <kuba@berezanscy.pl>
2020-03-11 21:16:23 +0100

    Add VS 2019 Remote Tools and Performance Tools

---
+++ visualstudio2019-performancetools/tools/chocolateyInstall.ps1
@@ -0,0 +1,25 @@
+﻿Set-StrictMode -Version 2
+$ErrorActionPreference = 'Stop'
+
+. (Join-Path -Path (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent) -ChildPath 'data.ps1')
+$packageName = $otherData.PackageName
+$installerType = 'exe'
+$silentArgs = '/quiet /norestart'
+$validExitCodes = @(0, 3010)
+
+Write-Verbose 'Checking Service Pack requirements'
+$os = Get-WmiObject -Class Win32_OperatingSystem
+$version = [Version]$os.Version
+if ($version -ge [Version]'6.1' -and $version -lt [Version]'6.2' -and $os.ServicePackMajorVersion -lt 1)
+{
+  # On Windows 7 / Server 2008 R2, Service Pack 1 is required.
+  throw 'This package requires Service Pack 1 to be installed first. The "KB976932" package may be used to install it.'
+}
+
+Set-StrictMode -Off
+Install-ChocolateyPackage -PackageName $packageName `
+                          -FileType $installerType `
+                          -SilentArgs $silentArgs `
+                          -ValidExitCodes $validExitCodes `
+                          @installData32 `
+                          @installData64
---
+++ visualstudio2019-remotetools/tools/data.ps1
@@ -0,0 +1,19 @@
+﻿$installData32 = @{
+  Url = 'https://download.visualstudio.microsoft.com/download/pr/208cc0c2-6455-4c15-86b2-33a54ed54739/bac1d64ecf94f0f61eb75fcf27fcaa60600d3f2f4f99558d8c0e01da651545e2/VS_RemoteTools.exe'
+  Checksum = 'BAC1D64ECF94F0F61EB75FCF27FCAA60600D3F2F4F99558D8C0E01DA651545E2'
+  ChecksumType = 'sha256'
+}
+
+$installData64 = @{
+  Url64 = 'https://download.visualstudio.microsoft.com/download/pr/208cc0c2-6455-4c15-86b2-33a54ed54739/7ec6144f19125bcf1c2c32f5cd8758846172f38528dd93a889de4177f2ef3d27/VS_RemoteTools.exe'
+  Checksum64 = '7EC6144F19125BCF1C2C32F5CD8758846172F38528DD93A889DE4177F2EF3D27'
+  ChecksumType64 = 'sha256'
+}
+
+$uninstallData = @{
+  SoftwareName = 'Remote Tools for Visual Studio 2019'
+}
+
+$otherData = @{
+  PackageName = 'visualstudio2019-remotetools'
+}
