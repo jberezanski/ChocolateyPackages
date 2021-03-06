7695ced256c73870e2afd5f603359657622bc35e
Jakub Bereżański <kuba@berezanscy.pl>
2021-03-06 22:56:19 +0100

    chocolatey-visualstudio.extension: implement obtaining ChannelProduct items from channel manifest

--- chocolatey-visualstudio.extension/extensions/Get-VSChannelManifestItem.ps1
+++ chocolatey-visualstudio.extension/extensions/Get-VSChannelManifestItem.ps1
@@ -1,33 +1,57 @@
 function Get-VSChannelManifestItem
 {
+    [OutputType([Collections.IDictionary])]
     [CmdletBinding()]
     Param
     (
         [Parameter(Mandatory = $true)] [System.Collections.IDictionary] $Manifest,
-        [ValidateSet('Bootstrapper', 'Manifest')] [Parameter(Mandatory = $true)] [string] $ChannelItemType
+        [ValidateSet('Bootstrapper', 'Manifest', 'ChannelProduct')] [Parameter(Mandatory = $true)] [string] $ChannelItemType,
+        [string] $Id
     )
 
-    $result = $null
+    if ($Id -eq '')
+    {
+        $searchDesc = "type $ChannelItemType"
+        $additionalFilter = { $true }
+        $expectSingle = $ChannelItemType -ne 'ChannelProduct'
+    }
+    else
+    {
+        $searchDesc = "type $ChannelItemType and id $Id"
+        $additionalFilter = { $_.ContainsKey('id') -and $_['id'] -eq $Id }
+        $expectSingle = $true
+    }
+
+    $totalCount = 0
     if ($Manifest -is [Collections.IDictionary] -and $Manifest.ContainsKey('channelItems'))
     {
         $channelItems = $Manifest['channelItems']
         if ($channelItems -is [array])
         {
-            $channelItem = $channelItems | Where-Object { $_ -is [Collections.IDictionary] -and $_.ContainsKey('type') -and $_['type'] -eq $ChannelItemType }
-            if (($channelItem | Measure-Object).Count -eq 1)
+            $matchingItems = @($channelItems | Where-Object { $_ -is [Collections.IDictionary] -and $_.ContainsKey('type') -and $_['type'] -eq $ChannelItemType } | Where-Object $additionalFilter)
+            $matchingItemsCount = ($matchingItems | Measure-Object).Count
+            if ($matchingItemsCount -eq 0)
             {
-                if ($channelItem -is [Collections.IDictionary])
-                {
-                    $result = $channelItem
-                }
-                else
-                {
-                    Write-Debug 'Manifest parsing error: channelItem is not IDictionary'
-                }
+                Write-Debug "Manifest parsing error: zero channelItem objects found of $searchDesc"
+            }
+            elseif ($expectSingle -and $matchingItemsCount -gt 1)
+            {
+                Write-Debug "Manifest parsing error: expected 1 but found $matchingItemsCount channelItem objects of $searchDesc"
             }
             else
             {
-                Write-Debug 'Manifest parsing error: zero or more than one channelItem objects found'
+                foreach ($channelItem in $matchingItems)
+                {
+                    if ($channelItem -is [Collections.IDictionary])
+                    {
+                        Write-Output $channelItem
+                        $totalCount += 1
+                    }
+                    else
+                    {
+                        Write-Debug 'Manifest parsing error: channelItem is not IDictionary'
+                    }
+                }
             }
         }
         else
@@ -40,14 +64,12 @@ function Get-VSChannelManifestItem
         Write-Debug 'Manifest parsing error: manifest is not IDictionary or does not contain channelItems'
     }
 
-    if ($null -ne $result)
+    if ($totalCount -ne 0)
     {
-        Write-Debug "Located channel manifest item of type $ChannelItemType"
+        Write-Debug "Located $totalCount channel manifest item(s) of $searchDesc"
     }
     else
     {
-        Write-Debug "Could not locate the channel manifest item of type $ChannelItemType"
+        Write-Debug "Could not locate any channel manifest item(s) of $searchDesc"
     }
-
-    return $result
 }
