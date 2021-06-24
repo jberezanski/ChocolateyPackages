ec81ee30409e56a01cf46192b132f7ff206dfa54
Jakub Bereżański <kuba@berezanscy.pl>
2021-06-25 01:17:48 +0200

    add visualstudio2022enterprise-preview

---
+++ visualstudio2022enterprise-preview/tools/ChocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿Install-VisualStudio `
+    -PackageName 'visualstudio2022enterprise-preview' `
+    -ApplicationName 'Microsoft Visual Studio Enterprise 2022 Preview' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/TBD/vs_Enterprise.exe' <# https://aka.ms/vs/17/pre/vs_enterprise.exe #> `
+    -Checksum '' `
+    -ChecksumType 'SHA256' `
+    -InstallerTechnology 'WillowVS2017OrLater' `
+    -Product 'Enterprise' `
+    -VisualStudioYear '2022' `
+    -Preview $true
---
+++ visualstudio2022enterprise-preview/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,5 @@
+﻿Remove-VisualStudioProduct `
+    -PackageName 'visualstudio2022enterprise-preview' `
+    -Product 'Enterprise' `
+    -VisualStudioYear '2022' `
+    -Preview $true
