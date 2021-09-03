77f90aac8d65f8b113b08daf94af9edef6a9d418
Jakub Bereżański <kuba@berezanscy.pl>
2021-09-03 17:55:18 +0200

    Add netfx-4.7.2, deprecate dotnet4.7.2 and fix netfx-4.7.2-devpack

--- netfx-4.7.2-devpack/tools/ChocolateyInstall.ps1
+++ netfx-4.7.2-devpack/tools/ChocolateyInstall.ps1
@@ -3,8 +3,8 @@ $arguments = @{
     PackageName = "netfx-$version-devpack"
     Version = $version
     ProductNameWithVersion = "Microsoft .NET Framework $version Developer Pack"
-    Url = 'https://download.microsoft.com/download/3/B/F/3BFB9C35-405D-45DF-BDAF-0EB57D047888/NDP472-DevPack-ENU.exe'
-    Checksum = '2F4EE2852B95C37A806E2DEEC567751DD59B0DD27049641BBD4E1C0E22ADFE46'
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/158dce74-251c-4af3-b8cc-4608621341c8/9c1e178a11f55478e2112714a3897c1a/ndp472-devpack-enu.exe'
+    Checksum = '878FDF9F137B1466855DE995C793B466CD50FCCC523D1F41250567973623180C'
     ChecksumType = 'sha256'
 }
 
---
+++ netfx-4.7.2/tools/ChocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$version = '4.7.2'
+$arguments = @{
+    PackageName = "netfx-$version"
+    Release = 461808
+    Version = $version
+    ProductNameWithVersion = "Microsoft .NET Framework $version"
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/1f5af042-d0e4-4002-9c59-9ba66bcf15f6/089f837de42708daacaae7c04b7494db/ndp472-kb4054530-x86-x64-allos-enu.exe'
+    Checksum = '5CB624B97F9FD6D3895644C52231C9471CD88AACB57D6E198D3024A1839139F6'
+    ChecksumType = 'sha256'
+}
+
+Install-DotNetFramework @arguments
