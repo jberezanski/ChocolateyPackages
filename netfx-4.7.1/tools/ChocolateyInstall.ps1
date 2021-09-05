60dd5602c2ced05a90c27cfa6f735fcd6aa8ea06
Jakub Bereżański <kuba@berezanscy.pl>
2021-09-05 19:01:51 +0200

    Add netfx-4.7 and netfx-4.7.1

---
+++ netfx-4.7.1/tools/ChocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$version = '4.7.1'
+$arguments = @{
+    PackageName = "netfx-$version"
+    Release = 461308
+    Version = $version
+    ProductNameWithVersion = "Microsoft .NET Framework $version"
+    Url = 'https://download.microsoft.com/download/9/E/6/9E63300C-0941-4B45-A0EC-0008F96DD480/NDP471-KB4033342-x86-x64-AllOS-ENU.exe'
+    Checksum = '63DC850DF091F3F137B5D4392F47917F847F8926DC8AF1DA9BFBA6422E495805'
+    ChecksumType = 'sha256'
+}
+
+Install-DotNetFramework @arguments
---
+++ netfx-4.7/tools/ChocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$version = '4.7'
+$arguments = @{
+    PackageName = "netfx-$version"
+    Release = 460798
+    Version = $version
+    ProductNameWithVersion = "Microsoft .NET Framework $version"
+    Url = 'https://download.microsoft.com/download/D/D/3/DD35CC25-6E9C-484B-A746-C5BE0C923290/NDP47-KB3186497-x86-x64-AllOS-ENU.exe'
+    Checksum = '24762159579EC9763BAEC8C23555464360BD31677EE8894A58BDB67262E7E470'
+    ChecksumType = 'sha256'
+}
+
+Install-DotNetFramework @arguments
