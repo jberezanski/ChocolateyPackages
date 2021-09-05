d834a15eb21fd3ccce6521b79688f730d5f2da54
Jakub Bereżański <kuba@berezanscy.pl>
2021-09-05 22:35:52 +0200

    netfx-4.6.2: update download url

--- netfx-4.6.2/tools/ChocolateyInstall.ps1
+++ netfx-4.6.2/tools/ChocolateyInstall.ps1
@@ -4,8 +4,8 @@ $arguments = @{
     Release = 394802
     Version = $version
     ProductNameWithVersion = "Microsoft .NET Framework $version"
-    Url = 'https://download.microsoft.com/download/F/9/4/F942F07D-F26F-4F30-B4E3-EBD54FABA377/NDP462-KB3151800-x86-x64-AllOS-ENU.exe'
-    Checksum = '28886593E3B32F018241A4C0B745E564526DBB3295CB2635944E3A393F4278D4'
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/8e396c75-4d0d-41d3-aea8-848babc2736a/80b431456d8866ebe053eb8b81a168b3/ndp462-kb3151800-x86-x64-allos-enu.exe'
+    Checksum = 'B4CBB4BC9A3983EC3BE9F80447E0D619D15256A9CE66FF414AE6E3856705E237'
     ChecksumType = 'sha256'
 }
 
