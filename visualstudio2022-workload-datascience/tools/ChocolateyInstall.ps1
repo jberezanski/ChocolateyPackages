3df8a6a14ed661f8f21a1532ed8667b766511143
Jakub Bereżański <kuba@berezanscy.pl>
2021-11-08 19:48:55 +0100

    remove another workload not present in VS 2022

--- visualstudio2022-workload-datascience/tools/ChocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿Add-VisualStudioWorkload `
-    -PackageName 'visualstudio2022-workload-datascience' `
-    -Workload 'DataScience' `
-    -VisualStudioYear '2022' `
-    -Preview $false `
-    -ApplicableProducts @('Community', 'Professional', 'Enterprise') `
-    -IncludeRecommendedComponentsByDefault
--- visualstudio2022-workload-datascience/tools/ChocolateyUninstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿Remove-VisualStudioWorkload `
-    -PackageName 'visualstudio2022-workload-datascience' `
-    -Workload 'DataScience' `
-    -VisualStudioYear '2022' `
-    -Preview $false `
-    -ApplicableProducts @('Community', 'Professional', 'Enterprise')
