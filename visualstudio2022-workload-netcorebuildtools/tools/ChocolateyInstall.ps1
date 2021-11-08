db6a95122dfc438582acaebbc9f8f4a40ddbb080
Jakub Bereżański <kuba@berezanscy.pl>
2021-11-08 19:39:08 +0100

    remove workloads not present in VS 2022

--- visualstudio2022-workload-netcorebuildtools/tools/ChocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿Add-VisualStudioWorkload `
-    -PackageName 'visualstudio2022-workload-netcorebuildtools' `
-    -Workload 'NetCoreBuildTools' `
-    -VisualStudioYear '2022' `
-    -Preview $false `
-    -ApplicableProducts @('BuildTools') `
-    -IncludeRecommendedComponentsByDefault
