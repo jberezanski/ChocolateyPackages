913e73935c1a6724d5691f6a9fd68cb4ed386ce2
Jakub Bereżański <kuba@berezanscy.pl>
2022-10-01 20:42:45 +0200

    Save current dotnetfx 4.8 to support future hotfixes

---
+++ dotnetfx_previous/dotnetfx-4.8/tools/ChocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$version = '4.8'
+$arguments = @{
+    PackageName = 'dotnetfx'
+    Release = 528040
+    Version = $version
+    ProductNameWithVersion = "Microsoft .NET Framework $version"
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/2d6bb6b2-226a-4baa-bdec-798822606ff1/8494001c276a4b96804cde7829c04d7f/ndp48-x86-x64-allos-enu.exe'
+    Checksum = '68C9986A8DCC0214D909AA1F31BEE9FB5461BB839EDCA996A75B08DDFFC1483F'
+    ChecksumType = 'sha256'
+}
+
+Install-DotNetFramework @arguments
