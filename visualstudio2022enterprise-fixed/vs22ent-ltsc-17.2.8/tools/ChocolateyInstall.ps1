b3acc55b0fa8ba1f7c7baeab1907975ff514f42b
Jakub Berezanski <kuba@berezanscy.pl>
2022-10-16 21:34:00 +0200

    DesiredProductVersion

--- visualstudio2022enterprise-fixed/vs22ent-ltsc-17.2.8/tools/ChocolateyInstall.ps1
+++ visualstudio2022enterprise-fixed/vs22ent-ltsc-17.2.8/tools/ChocolateyInstall.ps1
@@ -2,6 +2,7 @@
     -PackageName 'visualstudio2022enterprise' `
     -ApplicationName 'Microsoft Visual Studio Enterprise 2022' `
     -Url 'https://download.visualstudio.microsoft.com/download/pr/cc0385ef-f2fd-4e37-b989-8983ead82e53/aea0df277d828d9ecc944eba1b83f9a4d3d6a97620e5ed7d6b6b697822b457b3/vs_Enterprise.exe' `
+    -DesiredProductVersion '17.2.32901.213' `
     -Checksum '' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
