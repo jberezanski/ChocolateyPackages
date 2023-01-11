9c73860b9d968ea27e3098fd903efe71c2407bd2
Thomas Desveaux <thomas.desveaux@dont-nod.com>
2023-01-11 19:37:23 +0100

    Add VS 2022 Remote Tools

---
+++ visualstudio2022-remotetools/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿Set-StrictMode -Version 2
+$ErrorActionPreference = 'Stop'
+
+. (Join-Path -Path (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent) -ChildPath 'data.ps1')
+$packageName = $otherData.PackageName
+$installerType = 'exe'
+$silentArgs = '/quiet /norestart'
+$validExitCodes = @(0, 3010)
+
+Set-StrictMode -Off
+Install-ChocolateyPackage -PackageName $packageName `
+                          -FileType $installerType `
+                          -SilentArgs $silentArgs `
+                          -ValidExitCodes $validExitCodes `
+                          @installData32 `
+                          @installData64
