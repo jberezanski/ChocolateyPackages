625a6fe1c15bf6953da0f12230ec312b614b20a8
Jakub Bereżański <kuba@berezanscy.pl>
2024-04-21 13:52:34 +0200

    iisexpress: Update version and checksum

--- iisexpress/tools/ChocolateyInstall.ps1
+++ iisexpress/tools/ChocolateyInstall.ps1
@@ -8,10 +8,10 @@ $arguments = @{
         3010 # success, restart required
     )
     url = 'https://download.microsoft.com/download/C/E/8/CE8D18F5-D4C0-45B5-B531-ADECD637A1AA/iisexpress_x86_en-US.msi'
-    checksum = 'D51D6299EE266AE9180CCF2C48B0B4146E551FBFECA7CC83C840AB41078B8452'
+    checksum = '860B3F97C35E9E99B24C44FD110649680D45CFE6FE97E793ACF81DEAF0E373B2'
     checksumType = 'sha256'
     url64 = 'https://download.microsoft.com/download/C/E/8/CE8D18F5-D4C0-45B5-B531-ADECD637A1AA/iisexpress_amd64_en-US.msi'
-    checksum64 = '9FEE6C7DB937C215BBFBD35D62F9E8A5AE24C56551056B0325FFC1A63EB6B828'
+    checksum64 = '18304FE8A65E397C65FE77C6E73B0ACB1556E8ED7EC9C94678DD42FA7AC1671F'
     checksumType64 = 'sha256'
 }
 
