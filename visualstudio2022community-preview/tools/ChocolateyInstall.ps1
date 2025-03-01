aba3fc0b014ecd03b9f7fe5113b1769b9afa133a
Jakub Bereżański <kuba@berezanscy.pl>
2025-03-01 14:35:10 +0100

    visualstudio2022*-preview: update to 17.14.0 Preview 1.1

--- visualstudio2022community-preview/tools/ChocolateyInstall.ps1
+++ visualstudio2022community-preview/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿Install-VisualStudio `
     -PackageName 'visualstudio2022community-preview' `
     -ApplicationName 'Microsoft Visual Studio Community 2022 Preview' `
-    -Url 'https://download.visualstudio.microsoft.com/download/pr/fc02efcc-956d-493d-bef7-ca5e3bdcf0d1/616f9de0f5c9078a81ef7514c73f2ad008b5f39104dd21a6199901979d75d613/vs_Community.exe' <# https://aka.ms/vs/17/pre/vs_community.exe #> `
-    -Checksum '616F9DE0F5C9078A81EF7514C73F2AD008B5F39104DD21A6199901979D75D613' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/0438db17-1ea0-46d0-831c-a66c46af9477/ca2417692bbb21d8a9262a74862667566c3e2e1759e02638507108bbfbe9590c/vs_Community.exe' <# https://aka.ms/vs/17/pre/vs_community.exe #> `
+    -Checksum 'CA2417692BBB21D8A9262A74862667566C3E2E1759E02638507108BBFBE9590C' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
     -Product 'Community' `
--- visualstudio2022testcontroller-preview/tools/ChocolateyInstall.ps1
+++ visualstudio2022testcontroller-preview/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿Install-VisualStudio `
     -PackageName 'visualstudio2022testcontroller-preview' `
     -ApplicationName 'Microsoft Visual Studio Load Test Controller 2022 Preview' `
-    -Url 'https://download.visualstudio.microsoft.com/download/pr/fc02efcc-956d-493d-bef7-ca5e3bdcf0d1/a358e66cc540df2a796efde502576bdf7c88292ace7b8011a8b769b612e6e888/vs_TestController.exe' <# https://aka.ms/vs/17/pre/vs_testcontroller.exe #> `
-    -Checksum 'A358E66CC540DF2A796EFDE502576BDF7C88292ACE7B8011A8B769B612E6E888' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/0438db17-1ea0-46d0-831c-a66c46af9477/e31d3a087758b45d1c4665f50ca3d5ca7f148cc08cc402bf4370c03f6896da71/vs_TestController.exe' <# https://aka.ms/vs/17/pre/vs_testcontroller.exe #> `
+    -Checksum 'E31D3A087758B45D1C4665F50CA3D5CA7F148CC08CC402BF4370C03F6896DA71' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
     -Product 'TestController' `
