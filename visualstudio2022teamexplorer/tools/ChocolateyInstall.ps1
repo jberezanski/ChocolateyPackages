eb969de5cff70c0f8c0f1590837cec0ea1a59ed3
Jakub Bereżański <kuba@berezanscy.pl>
2025-03-01 14:34:47 +0100

    visualstudio2022*: update to 17.13.2

--- visualstudio2022teamexplorer/tools/ChocolateyInstall.ps1
+++ visualstudio2022teamexplorer/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿Install-VisualStudio `
     -PackageName 'visualstudio2022teamexplorer' `
     -ApplicationName 'Microsoft Visual Studio Team Explorer 2022' `
-    -Url 'https://download.visualstudio.microsoft.com/download/pr/6fb3e1c1-09c2-4ead-966e-fc94d48f5b17/aeaca942447dff638d6f9ea44c8962980305c1a0d2c767cde3c1b1adb56159eb/vs_TeamExplorer.exe' <# https://aka.ms/vs/17/release/vs_teamexplorer.exe #> `
-    -Checksum 'AEACA942447DFF638D6F9EA44C8962980305C1A0D2C767CDE3C1B1ADB56159EB' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/84955a63-15ca-4f52-94af-14ea55b50424/a31609583c9bb9eb83865df8be965a4634881b15809669ba7e945f9f820b2e21/vs_TeamExplorer.exe' <# https://aka.ms/vs/17/release/vs_teamexplorer.exe #> `
+    -Checksum 'A31609583C9BB9EB83865DF8BE965A4634881B15809669BA7E945F9F820B2E21' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
     -Product 'TeamExplorer' `
