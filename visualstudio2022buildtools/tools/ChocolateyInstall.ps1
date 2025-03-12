2c4a14123131b8afd85e9f9ac14b4222f73b019d
Jakub Bereżański <kuba@berezanscy.pl>
2025-03-13 00:09:35 +0100

    visualstudio2022*: update to 17.13.3

--- visualstudio2022buildtools/tools/ChocolateyInstall.ps1
+++ visualstudio2022buildtools/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿Install-VisualStudio `
     -PackageName 'visualstudio2022buildtools' `
     -ApplicationName 'Microsoft Visual Studio Build Tools 2022' `
-    -Url 'https://download.visualstudio.microsoft.com/download/pr/84955a63-15ca-4f52-94af-14ea55b50424/5493a860734ca0bc242deb81d940a418ea90f701feb64e7d9cdf0e8c591f4acf/vs_BuildTools.exe' <# https://aka.ms/vs/17/release/vs_buildtools.exe #> `
-    -Checksum '5493A860734CA0BC242DEB81D940A418EA90F701FEB64E7D9CDF0E8C591F4ACF' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/9b2a4ec4-2233-4550-bb74-4e7facba2e03/752b86394038f8e59e3e5ad7eec5d8fc1017f9b72c521565dc3ae0b0f36eef14/vs_BuildTools.exe' <# https://aka.ms/vs/17/release/vs_buildtools.exe #> `
+    -Checksum '752B86394038F8E59E3E5AD7EEC5D8FC1017F9B72C521565DC3AE0B0F36EEF14' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
     -Product 'BuildTools' `
--- visualstudio2022enterprise/tools/ChocolateyInstall.ps1
+++ visualstudio2022enterprise/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿Install-VisualStudio `
     -PackageName 'visualstudio2022enterprise' `
     -ApplicationName 'Microsoft Visual Studio Enterprise 2022' `
-    -Url 'https://download.visualstudio.microsoft.com/download/pr/84955a63-15ca-4f52-94af-14ea55b50424/10e598977cebf89d1f5a7fffe44cc16e449ad5ffdff18fd44ad1d1e1a568325d/vs_Enterprise.exe' <# https://aka.ms/vs/17/release/vs_enterprise.exe #> `
-    -Checksum '10E598977CEBF89D1F5A7FFFE44CC16E449AD5FFDFF18FD44AD1D1E1A568325D' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/9b2a4ec4-2233-4550-bb74-4e7facba2e03/c943116f21714c8c838415bd628b11a548a3d81f72494078f686068a4e3736b7/vs_Enterprise.exe' <# https://aka.ms/vs/17/release/vs_enterprise.exe #> `
+    -Checksum 'C943116F21714C8C838415BD628B11A548A3D81F72494078F686068A4E3736B7' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
     -Product 'Enterprise' `
