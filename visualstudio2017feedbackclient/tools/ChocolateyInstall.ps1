97392013edaf974a3e8c9b235b48241b6aecc9a6
Jakub Bereżański <kuba@berezanscy.pl>
2025-04-10 21:11:00 +0200

    visualstudio2017*: update to 15.9.72

--- visualstudio2017feedbackclient/tools/ChocolateyInstall.ps1
+++ visualstudio2017feedbackclient/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿Install-VisualStudio `
     -PackageName 'visualstudio2017feedbackclient' `
     -ApplicationName 'Microsoft Visual Studio Feedback Client 2017' `
-    -Url 'https://download.visualstudio.microsoft.com/download/pr/f157b16a-4b01-4fc4-931e-96f983f152c5/773e18a1de6b02e48a6ea699ffa6a8b96816c704217dfbe40f477fabaa214af2/vs_FeedbackClient.exe' `
-    -Checksum '773E18A1DE6B02E48A6EA699FFA6A8B96816C704217DFBE40F477FABAA214AF2' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/ea9d33ea-b2ed-49ec-9abb-64c341d539ca/32a5aeebbf7eafd52092fe63d338968890b3659daea4f7ed85fd8901fbc5d475/vs_FeedbackClient.exe' `
+    -Checksum '32A5AEEBBF7EAFD52092FE63D338968890B3659DAEA4F7ED85FD8901FBC5D475' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
     -Product 'FeedbackClient' `
--- visualstudio2017testcontroller/tools/ChocolateyInstall.ps1
+++ visualstudio2017testcontroller/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿Install-VisualStudio `
     -PackageName 'visualstudio2017testcontroller' `
     -ApplicationName 'Microsoft Visual Studio Test Controller 2017' `
-    -Url 'https://download.visualstudio.microsoft.com/download/pr/f157b16a-4b01-4fc4-931e-96f983f152c5/06117db9016a385b04bd38ac0fdef789c3351e77115ef4e38621e45fcb5ec299/vs_TestController.exe' `
-    -Checksum '06117DB9016A385B04BD38AC0FDEF789C3351E77115EF4E38621E45FCB5EC299' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/ea9d33ea-b2ed-49ec-9abb-64c341d539ca/40b9cd85b51a9a9d8bd7b8fb0b264832198238b6ad178c3a045e21e102fab56b/vs_TestController.exe' `
+    -Checksum '40B9CD85B51A9A9D8BD7B8FB0B264832198238B6AD178C3A045E21E102FAB56B' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
     -Product 'TestController' `
