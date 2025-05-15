49f74fc980d6a9994ca644eea165211a91277c3e
Jakub Bereżański <kuba@berezanscy.pl>
2025-05-15 09:34:30 +0200

    visualstudio2022*-preview: update to 17.14.0 Preview 7.0

--- visualstudio2022community-preview/tools/ChocolateyInstall.ps1
+++ visualstudio2022community-preview/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿Install-VisualStudio `
     -PackageName 'visualstudio2022community-preview' `
     -ApplicationName 'Microsoft Visual Studio Community 2022 Preview' `
-    -Url 'https://download.visualstudio.microsoft.com/download/pr/47d25f80-748f-47b3-9b87-9080d507e765/53b6a3123fede3350c4f8200996b44ad6d9dc306c0d8d34f4ed2cb6ffe49d1e9/vs_Community.exe' <# https://aka.ms/vs/17/pre/vs_community.exe #> `
-    -Checksum '53B6A3123FEDE3350C4F8200996B44AD6D9DC306C0D8D34F4ED2CB6FFE49D1E9' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/f50ab15d-99d5-43aa-b0b4-496b6cb1e574/65f5641b63de4c7c853fa1ee69fed419e79084d1d86e1a2740546f112d44a527/vs_Community.exe' <# https://aka.ms/vs/17/pre/vs_community.exe #> `
+    -Checksum '65F5641B63DE4C7C853FA1EE69FED419E79084D1D86E1A2740546F112D44A527' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
     -Product 'Community' `
--- visualstudio2022testagent-preview/tools/ChocolateyInstall.ps1
+++ visualstudio2022testagent-preview/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿Install-VisualStudio `
     -PackageName 'visualstudio2022testagent-preview' `
     -ApplicationName 'Microsoft Visual Studio Test Agent 2022 Preview' `
-    -Url 'https://download.visualstudio.microsoft.com/download/pr/47d25f80-748f-47b3-9b87-9080d507e765/1eea6d908a60ef5556c3601298f17f796f2570d29814adedf1e4577591c19173/vs_TestAgent.exe' <# https://aka.ms/vs/17/pre/vs_testagent.exe #> `
-    -Checksum '1EEA6D908A60EF5556C3601298F17F796F2570D29814ADEDF1E4577591C19173' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/f50ab15d-99d5-43aa-b0b4-496b6cb1e574/a4fa443d36909bfbe900efbf976738b4f66421648e26450b1c6265843abf8ee2/vs_TestAgent.exe' <# https://aka.ms/vs/17/pre/vs_testagent.exe #> `
+    -Checksum 'A4FA443D36909BFBE900EFBF976738B4F66421648E26450B1C6265843ABF8EE2' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
     -Product 'TestAgent' `
