ddcac3130890a09e602bd2ae103f5e20ce69b979
Jakub Bereżański <kuba@berezanscy.pl>
2025-10-24 21:59:04 +0200

    visualstudio2026*-preview: update to Insiders [11121.172]

--- visualstudio2026teamexplorer-preview/tools/ChocolateyInstall.ps1
+++ visualstudio2026teamexplorer-preview/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿Install-VisualStudio `
     -PackageName 'visualstudio2026teamexplorer-preview' `
     -ApplicationName 'Microsoft Visual Studio Team Explorer 2026 Insiders' `
-    -Url 'https://download.visualstudio.microsoft.com/download/pr/d365a0d7-db64-428b-8ce6-65ad5cecd7e0/53aa148d5bde06de28a7fab01621491ab57061c76bbcd39b20bffa26c1386bff/vs_TeamExplorer.exe' <# https://aka.ms/vs/18/insiders/vs_teamexplorer.exe #> `
-    -Checksum '53AA148D5BDE06DE28A7FAB01621491AB57061C76BBCD39B20BFFA26C1386BFF' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/39b7611e-81c3-43bd-8dd1-ac87a8be4188/80e16896a966303d89ba4e3f16588ee4d622c8f00cc712bd4666151c12270760/vs_TeamExplorer.exe' <# https://aka.ms/vs/18/insiders/vs_teamexplorer.exe #> `
+    -Checksum '80E16896A966303D89BA4E3F16588EE4D622C8F00CC712BD4666151C12270760' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
     -Product 'TeamExplorer' `
