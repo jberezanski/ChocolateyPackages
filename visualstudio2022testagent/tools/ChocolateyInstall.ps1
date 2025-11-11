bc52bd739edc8e9b493e282ab02b59275bbc399a
Jakub Bereżański <kuba@berezanscy.pl>
2025-11-12 00:31:36 +0100

    visualstudio2022*: update to 17.14.20

--- visualstudio2022testagent/tools/ChocolateyInstall.ps1
+++ visualstudio2022testagent/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿Install-VisualStudio `
     -PackageName 'visualstudio2022testagent' `
     -ApplicationName 'Microsoft Visual Studio Test Agent 2022' `
-    -Url 'https://download.visualstudio.microsoft.com/download/pr/5011a9cc-e8ef-42cb-ad72-87de1031accc/4d9fd547e52bd42034c46097608f3187ea120661943f46f5bbbabd82abd66231/vs_TestAgent.exe' <# https://aka.ms/vs/17/release/vs_testagent.exe #> `
-    -Checksum '4D9FD547E52BD42034C46097608F3187EA120661943F46F5BBBABD82ABD66231' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/fd2adb86-9bef-4b89-baea-d9613327cd2d/22145e125ecc5b6c0bc8f6d833bb916327749363f97cfe782cb1dd3f998a620a/vs_TestAgent.exe' <# https://aka.ms/vs/17/release/vs_testagent.exe #> `
+    -Checksum '22145E125ECC5B6C0BC8F6D833BB916327749363F97CFE782CB1DD3F998A620A' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
     -Product 'TestAgent' `
