dc9d290a29d74f529ecd018de13b14505d4a5b19
Jakub Bereżański <kuba@berezanscy.pl>
2026-01-14 02:18:18 +0100

    visualstudio2026*: update to 18.2.0

--- visualstudio2026enterprise/tools/ChocolateyInstall.ps1
+++ visualstudio2026enterprise/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿Install-VisualStudio `
     -PackageName 'visualstudio2026enterprise' `
     -ApplicationName 'Microsoft Visual Studio Enterprise 2026' `
-    -Url 'https://download.visualstudio.microsoft.com/download/pr/6efb3484-905b-485c-8b5f-9d3a5f39e731/47601d0157a62bf90c4caae53ae2ff50a3a9f4a11461cfc79fb4351420ec2648/vs_Enterprise.exe' <# https://aka.ms/vs/18/stable/vs_enterprise.exe #> `
-    -Checksum '47601D0157A62BF90C4CAAE53AE2FF50A3A9F4A11461CFC79FB4351420EC2648' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/83a2d131-5e82-4f5c-b9c3-a83a3d02012e/c03cb06245d3e34ed949506e660ab098525b6546e6a27380a7751acd8b07ca9c/vs_Enterprise.exe' <# https://aka.ms/vs/18/stable/vs_enterprise.exe #> `
+    -Checksum 'C03CB06245D3E34ED949506E660AB098525B6546E6A27380A7751ACD8B07CA9C' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
     -Product 'Enterprise' `
--- visualstudio2026professional/tools/ChocolateyInstall.ps1
+++ visualstudio2026professional/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿Install-VisualStudio `
     -PackageName 'visualstudio2026professional' `
     -ApplicationName 'Microsoft Visual Studio Professional 2026' `
-    -Url 'https://download.visualstudio.microsoft.com/download/pr/6efb3484-905b-485c-8b5f-9d3a5f39e731/e0f5bb256b8c10a5b1b0bfcfd3e1683133ce902a18ef6840038f9d43576acf8e/vs_Professional.exe' <# https://aka.ms/vs/18/stable/vs_professional.exe #> `
-    -Checksum 'E0F5BB256B8C10A5B1B0BFCFD3E1683133CE902A18EF6840038F9D43576ACF8E' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/83a2d131-5e82-4f5c-b9c3-a83a3d02012e/9a9c6b1201ed68722e8793dad351e2d4a33d6d9ed92bc15e063ea2111cb0374d/vs_Professional.exe' <# https://aka.ms/vs/18/stable/vs_professional.exe #> `
+    -Checksum '9A9C6B1201ED68722E8793DAD351E2D4A33D6D9ED92BC15E063EA2111CB0374D' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
     -Product 'Professional' `
