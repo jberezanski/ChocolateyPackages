87498d544ce51027fd1ebdc5129301ca66f867cf
Jakub Bereżański <kuba@berezanscy.pl>
2026-05-27 19:54:37 +0200

    Add visualstudio-servicefabrictools

---
+++ visualstudio-servicefabrictools/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  PackageName   = 'visualstudio-servicefabrictools'
+  VsixUrl       = 'https://ms-azuretools.gallerycdn.vsassets.io/extensions/ms-azuretools/microsoftvisualstudioazurefabricvsix/18.0.20260523.1/1779828492033/Microsoft.VisualStudio.Azure.Fabric.Vsix.vsix'
+  Checksum      = 'D56BCAFAAA4FF03DBA373D36519E0EB297FF081259A3D4B9D7FFE7D0354F1D7F'
+  ChecksumType  = 'sha256'
+}
+
+Install-VisualStudioVsixExtension @packageArgs
---
+++ visualstudio-servicefabrictools/tools/chocolateyUninstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageArgs = @{
+  PackageName   = 'visualstudio-servicefabrictools'
+  VsixId        = '462692d0-f184-44d2-b5d5-569566a551c8'
+}
+
+Uninstall-VisualStudioVsixExtension @packageArgs
