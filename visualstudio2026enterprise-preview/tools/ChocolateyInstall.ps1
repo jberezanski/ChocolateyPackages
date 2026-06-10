bb1d38787d8a548ec7320711bff40c01a89750f1
Jakub Bereżański <kuba@berezanscy.pl>
2026-06-10 21:52:42 +0200

    visualstudio2026*-preview: update to Insiders [11904.113]

--- visualstudio2026enterprise-preview/tools/ChocolateyInstall.ps1
+++ visualstudio2026enterprise-preview/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿Install-VisualStudio `
     -PackageName 'visualstudio2026enterprise-preview' `
     -ApplicationName 'Microsoft Visual Studio Enterprise 2026 Insiders' `
-    -Url 'https://download.visualstudio.microsoft.com/download/pr/a5832eeb-da50-49c6-a0b9-8532048ebb67/464cbf27d6a578de65dc83920deea047e301cfe67d7105d7c2f14d0c94357e72/vs_Enterprise.exe' <# https://aka.ms/vs/18/insiders/vs_enterprise.exe #> `
-    -Checksum '464CBF27D6A578DE65DC83920DEEA047E301CFE67D7105D7C2F14D0C94357E72' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/c50b43ac-3365-4ddc-9a52-7b104589358b/0adcf32370e7a833cdc16aa99df1848c70a2f26e5341c4ceb539ff86884d6db3/vs_Enterprise.exe' <# https://aka.ms/vs/18/insiders/vs_enterprise.exe #> `
+    -Checksum '0ADCF32370E7A833CDC16AA99DF1848C70A2F26E5341C4CEB539FF86884D6DB3' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
     -Product 'Enterprise' `
--- visualstudio2026professional-preview/tools/ChocolateyInstall.ps1
+++ visualstudio2026professional-preview/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿Install-VisualStudio `
     -PackageName 'visualstudio2026professional-preview' `
     -ApplicationName 'Microsoft Visual Studio Professional 2026 Insiders' `
-    -Url 'https://download.visualstudio.microsoft.com/download/pr/a5832eeb-da50-49c6-a0b9-8532048ebb67/0787edb18a9bf679ee99483c3bc9a5c19ebc1d970e4e0c9c637c5c4211f96cb0/vs_Professional.exe' <# https://aka.ms/vs/18/insiders/vs_professional.exe #> `
-    -Checksum '0787EDB18A9BF679EE99483C3BC9A5C19EBC1D970E4E0C9C637C5C4211F96CB0' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/c50b43ac-3365-4ddc-9a52-7b104589358b/c3f20b31e5c0e29a920961149c4f1fb8446db0a8e9e68ca6b8700a4f24bad95a/vs_Professional.exe' <# https://aka.ms/vs/18/insiders/vs_professional.exe #> `
+    -Checksum 'C3F20B31E5C0E29A920961149C4F1FB8446DB0A8E9E68CA6B8700A4F24BAD95A' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
     -Product 'Professional' `
